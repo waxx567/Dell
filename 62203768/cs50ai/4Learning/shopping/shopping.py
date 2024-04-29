657cac076de466da5752b5e5dcf8def02968b240
Wayne McRae <waynem567@gmail.com>
2024-04-29 08:41:05 +0200

    Update shopping.py

--- 62203768/cs50ai/4Learning/shopping/shopping.py
+++ 62203768/cs50ai/4Learning/shopping/shopping.py
@@ -60,20 +60,6 @@ def load_data(filename):
     is 1 if Revenue is true, and 0 otherwise.
     """
 
-    '''
-    with open(filename) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        line_count = 0
-        for row in csv_reader:
-            if line_count == 0:
-                print(f"Column names are {', '.join(row)}")
-                line_count += 1
-            else:
-                line_count += 1
-
-        print(f"Total: {line_count} lines")
-    '''
-
     with open(filename, newline='') as csv_file:
         reader = csv.DictReader(csv_file)
         for row in reader:
