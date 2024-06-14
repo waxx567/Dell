c329e8a43d0a4a0d42d244efb885bb155e6529b0
Wayne McRae <waynem567@gmail.com>
2024-06-14 07:59:49 +0200

    Update parser.py

--- 62203768/cs50ai/6Language/parser/parser.py
+++ 62203768/cs50ai/6Language/parser/parser.py
@@ -17,7 +17,7 @@ V -> "smiled" | "tell" | "were"
 
 # Set of context-free grammar rules for generating nonterminal symbols
 NONTERMINALS = """
-S -> NP V
+S -> NP V |
 
 AP -> Adj | AP Adj
 NP -> N | Det N | Det AP N | NP P NP | P NP
