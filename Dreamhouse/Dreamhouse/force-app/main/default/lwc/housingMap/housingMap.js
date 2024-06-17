795d8bc05e0042e7bc18768f41aaa84cfd6e654a
Wayne McRae <waynem567@gmail.com>
2024-06-17 12:36:27 +0200

    Update housingMap.js

--- Dreamhouse/Dreamhouse/force-app/main/default/lwc/housingMap/housingMap.js
+++ Dreamhouse/Dreamhouse/force-app/main/default/lwc/housingMap/housingMap.js
@@ -1,3 +1,12 @@
-import { LightningElement } from 'lwc';
-
-export default class HousingMap extends LightningElement {}
+import { LightningElement, wire } from "lwc";
+import getHouses from "@salesforce/apex/HouseService.getRecords";
+export default class HousingMap extends LightningElement {
+    mapMarkers;
+    error;
+    @wire(getHouses)
+    wiredHouses({ error, data }) {
+        if (data) {
+        console.log(data);
+    }
+  }
+}
