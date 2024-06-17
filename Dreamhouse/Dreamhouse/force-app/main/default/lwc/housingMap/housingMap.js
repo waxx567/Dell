63907122a774888e5c9ddb9d62aedbf3fa282cd7
Wayne McRae <waynem567@gmail.com>
2024-06-17 12:39:20 +0200

    Update housingMap.js

--- Dreamhouse/Dreamhouse/force-app/main/default/lwc/housingMap/housingMap.js
+++ Dreamhouse/Dreamhouse/force-app/main/default/lwc/housingMap/housingMap.js
@@ -6,7 +6,21 @@ export default class HousingMap extends LightningElement {
     @wire(getHouses)
     wiredHouses({ error, data }) {
         if (data) {
-        console.log(data);
+         // Use JavaScript Map function to transform the Apex method response wired to the component into the format required by lightning-map
+          this.mapMarkers = data.map((element) => {
+                return {
+                    location: {
+                        Street: element.Address__c,
+                        City: element.City__c,
+                        State: element.State__c
+                    },
+                    title: element.Name
+                };
+            });
+            this.error = undefined;
+        } else if (error) {
+            this.error = error;
+            this.mapMarkers = undefined;
+        }
     }
-  }
 }
