675cc3593618e875c3ef26dabf56ac6925e21162
Wayne McRae <waynem567@gmail.com>
2024-09-09 13:21:24 +0200

    Update main.js

--- Wayne McRae 3D portfolio/basic/main.js
+++ Wayne McRae 3D portfolio/basic/main.js
@@ -1,1 +1,13 @@
 import * as THREE from 'three';
+
+// 1. Create the scene
+
+// 2. Add the camera
+
+// 3. Create and add a cube object
+
+// 4. Add lighting
+
+// 5. Set up the renderer
+
+// 6. Animate the scene
