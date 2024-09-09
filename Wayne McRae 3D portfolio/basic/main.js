824caee60515be8f3e0941a27f2fa6344f6058d6
Wayne McRae <waynem567@gmail.com>
2024-09-09 13:28:39 +0200

    Update main.js

--- Wayne McRae 3D portfolio/basic/main.js
+++ Wayne McRae 3D portfolio/basic/main.js
@@ -1,8 +1,12 @@
 import * as THREE from 'three';
 
 // 1. Create the scene
+const scene: THREE.scene = new THREE.scene();
+scene.background = new THREE.Color('#F0F0F0');
 
 // 2. Add the camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000); 
+// For the Perspective Camera, 75 is the FOV, then comes the aspect ratio, then the near and far planes
 
 // 3. Create and add a cube object
 
