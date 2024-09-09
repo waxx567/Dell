429c7f2f0e16a3b02b6edd2b0530c1055330a852
Wayne McRae <waynem567@gmail.com>
2024-09-09 15:38:20 +0200

    Update main.js

--- Wayne McRae 3D portfolio/vite/main.js
+++ Wayne McRae 3D portfolio/vite/main.js
@@ -4,3 +4,10 @@ const canvas = document.getElementById('canvas');
 
 // 1. Scene
 const scene = new THREE.Scene();
+scene.background = new THREE.Color('#F0F0F0');
+
+// 2. Camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+
+// 3. Object
