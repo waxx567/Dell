440ab67def36968160d4dfc87731b6b3b97a59ba
Wayne McRae <waynem567@gmail.com>
2024-09-09 19:03:30 +0200

    Update main.js

--- Wayne McRae 3D portfolio/vite/main.js
+++ Wayne McRae 3D portfolio/vite/main.js
@@ -1,13 +1,13 @@
 import * as THREE from 'three';
 
-const canvas = document.getElementById( { elementId: 'canvas' } );
+const canvas = document.getElementById('canvas');
 
 // 1. Scene
 const scene = new THREE.Scene();
 scene.background = new THREE.Color('#F0F0F0');
 
 // 2. Camera
-const camera = new THREE.PerspectiveCamera( { fov: 75, aspect: window.innerWidth / window.innerHeight, near: 0.1, far: 1000 } );
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5;
 
 // 3. Object
@@ -15,7 +15,7 @@ const geometry = new THREE.DodecahedronGeometry();
 const material = new THREE.MeshBasicMaterial( { color: '#468585' } );
 const dodecahedron = new THREE.Mesh(geometry, material);
 
-const boxGeometry = new THREE.BoxGeometry( { width: 2, height: 0.1, depth: 2 } );
+const boxGeometry = new THREE.BoxGeometry(2, 0.1, 2);
 const boxMaterial = new THREE.MeshBasicMaterial( { color: '#B4B4B3' } );
 const box = new THREE.Mesh(boxGeometry, boxMaterial);
 box.position.y = -1.5;
@@ -25,7 +25,7 @@ scene.add(box);
 
 // 4. Light
 const light = new THREE.SpotLight( { color: 0x006769, intensity: 100 } );
-light.position.set( { x: 1, y: 1, z: 1 } );
+light.position.set(1, 1, 1);
 scene.add(light);
 
 // 5. Renderer
