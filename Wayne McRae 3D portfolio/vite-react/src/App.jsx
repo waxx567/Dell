c0391fbe1ea54cb897e4b7e65cc67819cb047d69
Wayne McRae <waynem567@gmail.com>
2024-09-10 13:01:59 +0200

    Update App.jsx

--- Wayne McRae 3D portfolio/vite-react/src/App.jsx
+++ Wayne McRae 3D portfolio/vite-react/src/App.jsx
@@ -2,7 +2,7 @@ import {Canvas, useFrame} from "@react-three/fiber";
 import {OrbitControls} from "@react-three/drei";
 
 const RotatingCube = () => {
-  const meshRef = useRef()
+  const meshRef = useRef();
 
   useFrame(() => {
     if(meshRef.current) {
