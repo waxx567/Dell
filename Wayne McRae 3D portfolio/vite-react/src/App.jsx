06fdbea310bc5033c6aa531221a32b32b004bcad
Wayne McRae <waynem567@gmail.com>
2024-09-10 13:16:06 +0200

    Update App.jsx

--- Wayne McRae 3D portfolio/vite-react/src/App.jsx
+++ Wayne McRae 3D portfolio/vite-react/src/App.jsx
@@ -1,6 +1,6 @@
 import { useRef } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, Sparkles } from "@react-three/drei";
 
 const RotatingCube = () => {
   const meshRef = useRef();
@@ -16,6 +16,8 @@ const RotatingCube = () => {
     <mesh ref={meshRef}>
       <cylinderGeometry args={[1, 1, 1]} />
       <meshLambertMaterial color="#468585" emissive="#468585" />
+
+      <Sparkles count={100} scale={1} size={6} speed={0.002} noise={0.2} color="orange" />
     </mesh>
   )
 }
