dc037ad30e8cdbcb0542ce4bfd34dfaa20758f80
Wayne McRae <waynem567@gmail.com>
2024-09-10 10:57:45 +0200

    Update App.jsx

--- Wayne McRae 3D portfolio/vite-react/src/App.jsx
+++ Wayne McRae 3D portfolio/vite-react/src/App.jsx
@@ -2,7 +2,7 @@ import { Canvas } from "@react-three/fiber";
 
 const App = () => {
   return (
-    <Canvas>
+    <Canvas style={{height: '100vh', width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
       Hello Three.js
     </Canvas>
   )
