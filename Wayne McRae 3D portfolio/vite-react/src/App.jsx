5875f8cf880bbd91b611dac6e17355a3c8f4941b
Wayne McRae <waynem567@gmail.com>
2024-09-10 11:17:03 +0200

    Update App.jsx

--- Wayne McRae 3D portfolio/vite-react/src/App.jsx
+++ Wayne McRae 3D portfolio/vite-react/src/App.jsx
@@ -5,6 +5,8 @@ const App = () => {
   return (
     <Canvas style={{ height: '100vh', width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
       <OrbitControls enableZoom={true} enablePan enableRotate />
+
+      <directionalLight position={[1, 1, 1]} intensity={10} color={0x9CDBA6} />
     </Canvas>
   )
 }
