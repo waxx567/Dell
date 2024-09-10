965053e826a9d5cdbcc46be5c6274a8e2d146ddd
Wayne McRae <waynem567@gmail.com>
2024-09-10 11:03:13 +0200

    Update App.jsx

--- Wayne McRae 3D portfolio/vite-react/src/App.jsx
+++ Wayne McRae 3D portfolio/vite-react/src/App.jsx
@@ -1,9 +1,10 @@
-import { Canvas } from "@react-three/fiber";
+import {Canvas} from "@react-three/fiber";
+import {OrbitControls} from "@react-three/drei";
 
 const App = () => {
   return (
-    <Canvas style={{height: '100vh', width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-      Hello Three.js
+    <Canvas style={{ height: '100vh', width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <OrbitControls enableZoom={true} enablePan enableRotate />
     </Canvas>
   )
 }
