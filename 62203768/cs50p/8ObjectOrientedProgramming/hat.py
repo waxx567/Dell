f569e23334ee6e6eaf6c6224f5e79290ad5568ec
Wayne McRae <waynem567@gmail.com>
2024-10-09 18:00:51 +0200

    Create hat.py

---
+++ 62203768/cs50p/8ObjectOrientedProgramming/hat.py
@@ -0,0 +1,10 @@
+# Wherein the idea of a sorting hat is implemented
+class Hat():
+    def sort(self, name):
+        print(name, "is in", "some house")
+
+
+# Instantiate a Hat object and store it in the hat variable
+hat = Hat()
+# The sorting hat has a method (function) inside it called sort
+hat.sort("Harry")
