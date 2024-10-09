da6a0ea914b6b5dccb3cf1c7ad2025b53338939f
Wayne McRae <waynem567@gmail.com>
2024-10-09 17:57:52 +0200

    Create test_jar.py

---
+++ 62203768/cs50p/8ObjectOrientedProgramming/jar/test_jar.py
@@ -0,0 +1,46 @@
+'''
+Either before or after you implement jar.py, additionally implement, in a file called test_jar.py, four or more functions that collectively test your implementation of Jar thoroughly, each of whose names should begin with test_ so that you can execute your tests with:
+
+pytest test_jar.py
+
+Note that it’s not as easy to test instance methods as it is to test functions alone, since instance methods sometimes manipulate the same “state” (i.e., instance variables). To test one method (e.g., withdraw), then, you might need to call another method first (e.g., deposit). But the method you call first might itself not be correct!
+
+And so programmers sometimes mock (i.e., simulate) state when testing methods, as with Python’s own mock object library, so that you can call just the one method but modify the underlying state first, without calling the other method to do so.
+
+For simplicity, though, no need to mock any state. Implement your tests as you normally would!
+'''
+
+from jar import Jar
+
+
+def test_init():
+    jar = Jar()
+    assert jar.capacity == 12
+    jar_b = Jar(2)
+    assert jar_b.capacity == 2
+
+
+def test_str():
+    jar = Jar()
+    assert str(jar) == ""
+    jar.deposit(1)
+    assert str(jar) == "🍪"
+    jar.deposit(11)
+    assert str(jar) == "🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪"
+
+
+def test_deposit():
+    jar = Jar()
+    jar.deposit(3)
+    assert jar.size == 3
+    jar.deposit(1)
+    assert jar.size == 4
+
+
+def test_withdraw():
+    jar = Jar()
+    jar.deposit(5)
+    jar.withdraw(2)
+    assert jar.size == 3
+    jar.withdraw(2)
+    assert jar.size == 1
