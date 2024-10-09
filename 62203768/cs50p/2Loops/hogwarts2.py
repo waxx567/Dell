d814e5b0feccdfaf6aefca2030f2d8f6e4e56133
Wayne McRae <waynem567@gmail.com>
2024-10-09 16:43:47 +0200

    Create hogwarts2.py

---
+++ 62203768/cs50p/2Loops/hogwarts2.py
@@ -0,0 +1,11 @@
+# For multiple keys in dicts
+
+students = [
+    {"name": "Hermione", "house": "Gryffindor", "patronus": "Otter"},
+    {"name": "Harry", "house": "Gryffindor", "patronus": "Stag"},
+    {"name": "Ron", "house": "Gryffindor", "patronus": "Jack Russell terrier"},
+    {"name": "Draco", "house": "Slytherin", "patronus": None}
+]
+
+for student in students:
+    print(student["name"], student["house"], student["patronus"], sep=", ")
