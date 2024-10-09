4f69d3af1c82d32d4aba3b11e2a1929eefb5d1be
Wayne McRae <waynem567@gmail.com>
2024-10-09 18:05:37 +0200

    Create student.py

---
+++ 62203768/cs50p/8ObjectOrientedProgramming/student.py
@@ -0,0 +1,7 @@
+# A simple program that gets a name and house from the user and returns it
+name = input("Name: ")
+house = input("House: ")
+print(f"{name} from {house}")
+
+# Type Harry for name
+# And Griffyndor for house
