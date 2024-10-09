4b1250517ed3374eea445ba0831db1dcf761f653
Wayne McRae <waynem567@gmail.com>
2024-10-09 16:17:32 +0200

    Create bank.py

---
+++ 62203768/cs50p/1Conditionals/bank/bank.py
@@ -0,0 +1,22 @@
+''' 
+In a file called bank.py, implement a program that prompts the user for a greeting.
+If the greeting starts with “hello”, output $0.
+If the greeting starts with an “h” (but not “hello”), output $20.
+Otherwise, output $100. Ignore any leading whitespace in the user’s greeting,
+and treat the user’s greeting case-insensitively.
+'''
+
+# Prompt user
+answer = input("Greeting: ").strip().lower()
+
+# Check answer and output relevant result
+if "hello" in answer:
+    print("$0")
+else:
+    for i in answer:
+        if answer[0] == "h":
+            print("$20")
+            break
+        else:
+            print("$100")
+            break
