cf1cbaa1d281c5f37b5d0dd75b357b02a1e87e84
Wayne McRae <waynem567@gmail.com>
2024-10-09 16:04:26 +0200

    Create indoor.py

---
+++ 62203768/cs50p/0FunctionsVariables/indoor/indoor.py
@@ -0,0 +1,12 @@
+'''
+Given a string, convert all to lowercase
+'''
+
+# Get user input and store in shout variable
+shout = input("Shout something: ")
+
+# Convert shout variable to lowercase and store in whisper variable
+whisper = shout.lower()
+
+# Print whisper with required text
+print(f"Whispered back:  {whisper}")
