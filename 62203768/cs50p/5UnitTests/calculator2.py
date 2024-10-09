95ac89ee9e609bef8967fcbcfe9687345c162a70
Wayne McRae <waynem567@gmail.com>
2024-10-09 17:33:02 +0200

    Create calculator2.py

---
+++ 62203768/cs50p/5UnitTests/calculator2.py
@@ -0,0 +1,10 @@
+def main():
+    x = input("x: ")
+    print("x2 is", square(x))
+
+def square(n):
+    return n * n
+# to test, change the multiplication sign above to a plus sign
+
+if __name__ == "__main__":
+    main()
