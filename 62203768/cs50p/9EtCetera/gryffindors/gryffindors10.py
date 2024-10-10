80aa4bb4277dd23613873d8d80deb5cb06bffd67
Wayne McRae <waynem567@gmail.com>
2024-10-10 07:44:15 +0200

    Create gryffindors10.py

---
+++ 62203768/cs50p/9EtCetera/gryffindors/gryffindors10.py
@@ -0,0 +1,10 @@
+# enumerate
+students = ["Hermione", "Harry", "Ron"]
+
+for i, student in enumerate(students):
+    print(i + 1, student)
+
+# python gryffindors10.py
+# 1 Hermione
+# 2 Harry
+# 3 Ron
