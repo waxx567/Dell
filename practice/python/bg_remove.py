ed644e8a1d2094b01f0cf82031ba7b4385d258a0
Wayne McRae <waynem567@gmail.com>
2024-10-10 14:56:18 +0200

    Create bg_remove.py

---
+++ practice/python/bg_remove.py
@@ -0,0 +1,12 @@
+# pip install rembg
+
+from rembg import remove
+from PIL import Image
+
+input_path = "cl.jpg"
+output_path = "output.png"
+
+input = Image.open(input_path)
+output = remove(input)
+
+output.save(output_path)
