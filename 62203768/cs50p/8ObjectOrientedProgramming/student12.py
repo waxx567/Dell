1c1e6869ac9ba00555aee5c4dfd2d931abf108e5
Wayne McRae <waynem567@gmail.com>
2024-10-10 07:04:23 +0200

    Create student12.py

---
+++ 62203768/cs50p/8ObjectOrientedProgramming/student12.py
@@ -0,0 +1,25 @@
+# If user does not input a name or one of the four houses
+# Classes encourage you to encapsulate all of the functionality associated with that class inside of that class ie. keep all of the related code together
+class Student:
+    def __init__(self, name, house):
+        if not name:
+            raise ValueError("Missing name")
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
+        self.name = name
+        self.house = house
+
+
+def main():
+    student = get_student()
+    print(f"{student.name} from {student.house}")
+
+
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    return Student(name, house)
+
+
+if __name__ == "__main__":
+    main()
