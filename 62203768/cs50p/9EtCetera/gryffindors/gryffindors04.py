a6985f0ac15f9ea46383aa483b08d8c0b1702342
Wayne McRae <waynem567@gmail.com>
2024-10-10 07:41:15 +0200

    Create gryffindors04.py

---
+++ 62203768/cs50p/9EtCetera/gryffindors/gryffindors04.py
@@ -0,0 +1,18 @@
+students = [
+    {"name": "Hermione", "house": "Gryffindor"},
+    {"name": "Harry", "house": "Gryffindor"},
+    {"name": "Ron", "house": "Gryffindor"},
+    {"name": "Draco", "house": "Slytherin"},
+]
+
+gryffindors = filter(lambda s: s["house"] == "Gryffindor", students)
+
+for gryffindor in sorted(gryffindors, key=lambda s: s["name"]):
+    print(gryffindor["name"])
+
+# python gryffindors04.py
+# Harry
+# Hermione
+# Ron
+
+# use lambda function if a function is used in one and only one place
