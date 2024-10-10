90ca568eacf253b12e7e46eac64d442b4b7432fa
Wayne McRae <waynem567@gmail.com>
2024-10-10 08:08:05 +0200

    Create yell.py

---
+++ 62203768/cs50p/9EtCetera/yell/yell.py
@@ -0,0 +1,15 @@
+# Prints a phrase in uppercase
+def main():
+    yell("This is CS50")
+
+
+def yell(phrase):
+    print(phrase.upper())
+
+
+if __name__ == "__main__":
+    main()
+
+
+# python yell.py
+# THIS IS CS50
