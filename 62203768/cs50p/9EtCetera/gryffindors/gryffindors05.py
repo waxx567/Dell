000f64eba052c83ee2fa6ff81057e6e4924975df
Wayne McRae <waynem567@gmail.com>
2024-10-10 07:41:47 +0200

    Create gryffindors05.py

---
+++ 62203768/cs50p/9EtCetera/gryffindors/gryffindors05.py
@@ -0,0 +1,12 @@
+# say
+students = ["Hermione", "Harry", "Ron"]
+
+gryffindors = []
+
+for student in students:
+    gryffindors.append({"name": student, "house": "Gryffindor"})
+
+print(gryffindors)
+
+# python gryffindors05.py
+# [{'name': 'Hermione', 'house': 'Gryffindor'}, {'name': 'Harry', 'house': 'Gryffindor'}, {'name': 'Ron', 'house': 'Gryffindor'}]
