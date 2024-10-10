86afd2773c682074843f2ad57acc999f79885fec
Wayne McRae <waynem567@gmail.com>
2024-10-10 07:43:12 +0200

    Create gryffindors08.py

---
+++ 62203768/cs50p/9EtCetera/gryffindors/gryffindors08.py
@@ -0,0 +1,12 @@
+# enumerate
+students = ["Hermione", "Harry", "Ron"]
+
+for student in students:
+    print(student)
+
+# python gryffindors08.py
+# Hermione
+# Harry
+# Ron
+
+# not helpful if you want the student's rank or position in the list
