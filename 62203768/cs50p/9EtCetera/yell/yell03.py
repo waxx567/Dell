a5cf05e23ec7a72860b48afa5a1f8bb8dc836b79
Wayne McRae <waynem567@gmail.com>
2024-10-10 08:10:33 +0200

    Create yell03.py

---
+++ 62203768/cs50p/9EtCetera/yell/yell03.py
@@ -0,0 +1,18 @@
+# Removing the brackets and allowing for an undetermined number of args
+def main():
+    yell("This", "is", "CS50")
+
+
+def yell(*words):
+    uppercased = []
+    for word in words:
+        uppercased.append(word.upper())
+    print(*uppercased)
+
+
+if __name__ == "__main__":
+    main()
+
+
+# python yell03.py
+# THIS IS CS50
