cc1f73cb9a11ba90c7d4a33fbd104b33f04712a0
Wayne McRae <waynem567@gmail.com>
2024-10-10 08:06:08 +0200

    Create unpack04.py

---
+++ 62203768/cs50p/9EtCetera/unpack/unpack04.py
@@ -0,0 +1,19 @@
+# Unpacking a dictionary
+def total(galleons, sickles, knuts):
+    return (galleons * 17 + sickles) * 29 + knuts
+
+coins = {"galleons": 100, "sickles": 50, "knuts": 25}
+
+print(total(coins["galleons"], coins["sickles"], coins["knuts"]), "Knuts")
+# This is the old way - very verbose
+
+
+# This is how to unpack a dict
+def total(galleons, sickles, knuts):
+    return (galleons * 17 + sickles) * 29 + knuts
+
+coins = {"galleons": 100, "sickles": 50, "knuts": 25}
+
+print(total(**coins), "Knuts")
+# This final line is now the equivalent of passing in:
+print(total(galleons=100, sickles=50, knuts=25), "Knuts")
