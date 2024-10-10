a63768a86e160c208823eafeaeebd01e5db00ea6
Wayne McRae <waynem567@gmail.com>
2024-10-10 08:03:44 +0200

    Create unpack.py

---
+++ 62203768/cs50p/9EtCetera/unpack/unpack.py
@@ -0,0 +1,8 @@
+# first, last = input("What's your name: ").split(' ')
+# since we're not using the last name
+first, _ = input("What's your name: ").split(' ')
+print(f"hello, {first}")
+
+# python unpack.py
+# What's your name: David Malan
+# hello, David
