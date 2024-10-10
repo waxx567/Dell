f29a6d3a4931d9f5c1859e2c2347713774de037e
Wayne McRae <waynem567@gmail.com>
2024-10-10 07:49:49 +0200

    Create meows06.py

---
+++ 62203768/cs50p/9EtCetera/meows/meows06.py
@@ -0,0 +1,9 @@
+def meow(n: int):
+    for _ in range(n):
+        print("meow")
+
+
+number: int = int(input("Number: "))
+meow(number)
+
+# Now it works (try and except error checking notwithstanding)
