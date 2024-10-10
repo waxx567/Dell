545f36b64d5feaca7e4f873456b596b173eecd35
Wayne McRae <waynem567@gmail.com>
2024-10-10 07:15:06 +0200

    Create bank.py

---
+++ 62203768/cs50p/9EtCetera/bank/bank.py
@@ -0,0 +1,14 @@
+balance = 0
+
+
+def main():
+    print("Balance: ", balance)
+
+
+if __name__ == "__main__":
+    main()
+
+# run python bank.py
+# Balance: 0
+
+# This works even though the balance variable is assigned outside of the main function but only because it is not changing
