692c18974367103c51d9d480b05a613f3646026b
Wayne McRae <waynem567@gmail.com>
2024-10-10 07:46:48 +0200

    Create meows.py

---
+++ 62203768/cs50p/9EtCetera/meows/meows.py
@@ -0,0 +1,12 @@
+# for i in range(3):
+    # print("meow")
+# or since there is no i
+
+for _ in range(3):
+    print("meow")
+
+# 3 is hard-coded - no problem here but not good in bigger programs
+
+# meow
+# meow
+# meow
