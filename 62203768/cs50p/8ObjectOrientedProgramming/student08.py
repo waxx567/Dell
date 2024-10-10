13b97fef79e8524a17ec578d03b572043993cad4
Wayne McRae <waynem567@gmail.com>
2024-10-10 07:02:07 +0200

    Create student08.py

---
+++ 62203768/cs50p/8ObjectOrientedProgramming/student08.py
@@ -0,0 +1,16 @@
+# Solving the Padma problem is easy
+def main():
+    student = get_student()
+    if student["name"] == "Padma":
+        student["house"] = "Ravenclaw"
+    print(f"{student['name']} from {student['house']}")
+
+
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    return {"name": name, "house": house}
+
+
+if __name__ == "__main__":
+    main()
