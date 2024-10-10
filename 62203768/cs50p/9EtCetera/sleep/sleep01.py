da226520c7982de0ead6f3c49d0c425c86ed0604
Wayne McRae <waynem567@gmail.com>
2024-10-10 08:01:11 +0200

    Create sleep01.py

---
+++ 62203768/cs50p/9EtCetera/sleep/sleep01.py
@@ -0,0 +1,16 @@
+# Generators, iterators, yield
+def main():
+    n = int(input("What's n? "))
+    for i in range(n):
+        print("🐑" * i)
+
+
+if __name__ == "__main__":
+    main()
+
+
+# python sleep01.py
+# What's n? 3
+# 🐑
+# 🐑🐑
+# 🐑🐑🐑
