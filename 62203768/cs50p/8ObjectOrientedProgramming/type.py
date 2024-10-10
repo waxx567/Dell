c5fe09d14bfc88617cc343e88efc7c5b5597d393
Wayne McRae <waynem567@gmail.com>
2024-10-10 07:10:27 +0200

    Create type.py

---
+++ 62203768/cs50p/8ObjectOrientedProgramming/type.py
@@ -0,0 +1,14 @@
+# Types of classes
+
+print(type(50))
+# <class 'int'>
+print(type("hello, world"))
+# <class 'str'>
+print(type([]))
+# <class 'list'>
+print(type(list()))
+# <class 'list'>
+print(type({}))
+# <class 'dict'>
+print(type(dict()))
+# <class 'dict'>
