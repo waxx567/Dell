b8b16e1639e7b9ab388b646ab3623742b2ed22d7
Wayne McRae <waynem567@gmail.com>
2024-10-14 12:48:04 +0200

    Update calendar.py

--- python/calendar.py
+++ python/calendar.py
@@ -6,4 +6,4 @@ print(calendar(year, 2, 1, 8, 2))
 # 2 = 2 characters for days (Mo, Tu, etc)
 # 1 = 1 row for each week
 # 8 = 8 rows for each month
-# 2 = 2 columns for each month
+# 2 = 2 columns for all months
