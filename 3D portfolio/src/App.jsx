bc3b20095fa27adbbc04cf17e73634704eb4f60c
Wayne McRae <waynem567@gmail.com>
2024-10-15 17:38:08 +0200

    Navbar

--- 3D portfolio/src/App.jsx
+++ 3D portfolio/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from './sections/Navbar'
 
 const App = () => {
   return (
     <main className='max-w-7xl mx-auto'>
-      <h1 className='text-2xl text-white underline'>Hello, Three.js!</h1>
+      <Navbar />
     </main>
   )
 }
--- 3D portfolio/src/sections/Navbar.jsx
+++ 3D portfolio/src/sections/Navbar.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className='text-white'>
+      Navbar
     </div>
   )
 }
