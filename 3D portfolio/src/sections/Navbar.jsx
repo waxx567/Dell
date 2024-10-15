a57899bd3f0533fc770d61b3c838016beba23eb0
Wayne McRae <waynem567@gmail.com>
2024-10-15 19:23:13 +0200

    Update Navbar.jsx

--- 3D portfolio/src/sections/Navbar.jsx
+++ 3D portfolio/src/sections/Navbar.jsx
@@ -3,13 +3,7 @@ import { useState } from 'react';
 const navItems = () => {
     return (
         <ul className='nav-ul'>
-            <li className='nav-li'>
-                {['Home', 'About', 'Projects', 'Contact'].map((item, index) => (
-                    <li key={index} className='nav-li'>
-                        <a href='/' className='nav-li_a'>{item}</a>
-                    </li>
-                ))}
-            </li>
+            {}
         </ul>
     )
 }
