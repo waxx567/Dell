6c9fb8f8ed0222491e5525c47de33c459b28ff41
Wayne McRae <waynem567@gmail.com>
2024-10-15 19:38:45 +0200

    Update Navbar.jsx

--- 3D portfolio/src/sections/Navbar.jsx
+++ 3D portfolio/src/sections/Navbar.jsx
@@ -6,7 +6,9 @@ const navItems = () => {
         <ul className='nav-ul'>
             {navLinks.map(({ id, href, name }) => (
                 <li key={id} className='nav-li'>
-
+                    <a href={href} className='nav-li_a' onClick={() => {}}>
+                        {name}
+                    </a>
                 </li>
             ))}
         </ul>
