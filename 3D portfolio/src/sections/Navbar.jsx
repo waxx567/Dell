e7ae0f60328b385b413d144d033605635f988979
Wayne McRae <waynem567@gmail.com>
2024-10-15 18:17:20 +0200

    Update Navbar.jsx

--- 3D portfolio/src/sections/Navbar.jsx
+++ 3D portfolio/src/sections/Navbar.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <header className='fixed top-0 left-0 right-0 z-50 bg-black/90'>
         <div className='max-w-7xl mx-auto'>
