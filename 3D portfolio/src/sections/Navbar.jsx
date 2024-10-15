2b52bbaf4d19cc6d2e8fb2812b76b29297c59d5a
Wayne McRae <waynem567@gmail.com>
2024-10-15 18:22:53 +0200

    Update Navbar.jsx

--- 3D portfolio/src/sections/Navbar.jsx
+++ 3D portfolio/src/sections/Navbar.jsx
@@ -3,6 +3,8 @@ import React, { useState } from 'react'
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
+  const toggleMenu = () => setIsOpen(!isOpen);
+
   return (
     <header className='fixed top-0 left-0 right-0 z-50 bg-black/90'>
         <div className='max-w-7xl mx-auto'>
@@ -11,7 +13,7 @@ const Navbar = () => {
                     Wayne 
                 </a>
 
-                <button>
+                <button onClick={toggleMenu}>
                     <img src={isOpen ? 'assets/close.svg' : 'assets/menu.svg'} alt='toggle' className='w-6 h-6' />
                 </button>
             </div>
