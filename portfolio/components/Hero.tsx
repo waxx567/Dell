d45c9c51330bb67e3a2b34ba4013f7de0d3b9ff0
Wayne McRae <waynem567@gmail.com>
2024-10-16 15:38:30 +0200

    Update Hero.tsx

--- portfolio/components/Hero.tsx
+++ portfolio/components/Hero.tsx
@@ -13,6 +13,14 @@ const Hero = () => {
       {/* Radial gradient for the container to give a faded look */}
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
       </div>
+
+      <div className="flex justify-center">
+        <div className="max-w-[89vw]">
+          <h2>
+            Dynamic Web Magic with Next.js 
+          </h2>
+        </div>
+      </div>
     </div>
   )
 }
