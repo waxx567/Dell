a9d82f79e5c86885031763087b823e96450f2698
Wayne McRae <waynem567@gmail.com>
2024-10-16 09:19:38 +0200

    Update CanvasLoader.jsx

--- 3D portfolio/src/components/CanvasLoader.jsx
+++ 3D portfolio/src/components/CanvasLoader.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import { Html, useProgress } from '@react-three/drei'
 
 const CanvasLoader = () => {
+  const { progress } = useProgress()
+  
   return (
     <Html>
       
