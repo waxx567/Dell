d0c564831f5980c21d1d782ca9ff2ca746134b94
Wayne McRae <waynem567@gmail.com>
2024-10-16 07:40:06 +0200

    Update Hero.jsx

--- 3D portfolio/src/sections/Hero.jsx
+++ 3D portfolio/src/sections/Hero.jsx
@@ -1,6 +1,10 @@
 const Hero = () => {
   return (
-    <section className="min-h-screen w-full flex flex-col relative">Hero</section>
+    <section className="min-h-screen w-full flex flex-col relative">
+        <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+            <p>Hi, I'm Wayne <span>👋</span></p>
+        </div>
+    </section>
   )
 }
 
