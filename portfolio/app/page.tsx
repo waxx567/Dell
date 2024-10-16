af00dfd1efd39e673028b31a89d3639e340f8b34
Wayne McRae <waynem567@gmail.com>
2024-10-16 13:37:39 +0200

    Update page.tsx

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="relative">
+    <main className="relative bg-black">
       <div>
         <h1>Hello, Portfolio!</h1>
       </div>
