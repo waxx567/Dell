5c8547838e955821d5d05a06a568771508dfdd33
Wayne McRae <waynem567@gmail.com>
2024-10-16 14:32:20 +0200

    Update tailwind.config.ts

--- portfolio/tailwind.config.ts
+++ portfolio/tailwind.config.ts
@@ -1,21 +1,25 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{ts,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {
-      colors: {
-        black: {
-          DEFAULT: '#000000',
-          100: '#000319'
-        }
+      animation: {
+        spotlight: "spotlight 2s ease .75s 1 forwards",
+      },
+      keyframes: {
+        spotlight: {
+          "0%": {
+            opacity: 0,
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
       },
     },
   },
   plugins: [],
 };
-export default config;
