0ff45b0a717458424e153e91141bfce2eef91c04
Wayne McRae <waynem567@gmail.com>
2024-10-16 07:47:41 +0200

    Update Hero.jsx

--- 3D portfolio/src/sections/Hero.jsx
+++ 3D portfolio/src/sections/Hero.jsx
@@ -3,7 +3,7 @@ const Hero = () => {
     <section className="min-h-screen w-full flex flex-col relative">
         <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
             <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">Hi, I'm Wayne <span className="waving-hand">👋</span></p>
-            <p>Building Products & Brands</p>
+            <p className="hero_tag text-gray_gradient">Building Products & Brands</p>
         </div>
     </section>
   )
