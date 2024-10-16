246ace9a43f8acb38e06efaa5d5380c9dbf34509
Wayne McRae <waynem567@gmail.com>
2024-10-16 12:32:01 +0200

    target

--- 3D portfolio/src/sections/Hero.jsx
+++ 3D portfolio/src/sections/Hero.jsx
@@ -7,7 +7,7 @@ import { PerspectiveCamera } from '@react-three/drei';
 import Cube from '../components/Cube.jsx';
 import Rings from '../components/Rings.jsx';
 import ReactLogo from '../components/ReactLogo.jsx';
-// import Button from '../components/Button.jsx';
+import Button from '../components/Button.jsx';
 // import Target from '../components/Target.jsx';
 import CanvasLoader from '../components/CanvasLoader.jsx';
 // import HeroCamera from '../components/HeroCamera.jsx';
