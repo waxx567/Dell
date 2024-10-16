7a2050c064f89c0e5c34dc3653b3ab3719c5e356
Wayne McRae <waynem567@gmail.com>
2024-10-16 16:46:42 +0200

    Update Hero.tsx

--- portfolio/components/Hero.tsx
+++ portfolio/components/Hero.tsx
@@ -1,4 +1,5 @@
 import { Spotlight } from "./ui/Spotlight"
+import { TextGenerateEffect } from "./ui/TextGenerateEffect"
 
 const Hero = () => {
   return (
@@ -19,6 +20,8 @@ const Hero = () => {
           <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
             Dynamic Web Magic with Next.js 
           </h2>
+
+          <TextGenerateEffect  className="text-center text-[40px] md:text-5xl lg:text-6xl" words="Transforming Concepts into Seamless Experiences"/>
         </div>
       </div>
     </div>
