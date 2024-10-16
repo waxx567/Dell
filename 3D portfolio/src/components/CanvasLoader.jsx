387de53c03ac8cf37a7b25ab3ad1b9edaeeaf5e2
Wayne McRae <waynem567@gmail.com>
2024-10-16 12:08:49 +0200

    Update CanvasLoader.jsx

--- 3D portfolio/src/components/CanvasLoader.jsx
+++ 3D portfolio/src/components/CanvasLoader.jsx
@@ -1,21 +1,29 @@
-import { Html, useProgress } from '@react-three/drei'
+import { Html, useProgress } from '@react-three/drei';
 
 const CanvasLoader = () => {
-  const { progress } = useProgress()
-
+  const { progress } = useProgress();
   return (
-    <Html as='div' center style={{
+    <Html
+      as="div"
+      center
+      style={{
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        flexDirection: 'column'
-    }}>
-        <span className='canvas-loader' />
-        <p style={{ fontSize: 14, color: '#F1F1F1', fontWeight: 800, marginTop: 40 }}>
-            {progress != 0 ? `${progress.toFixed(2)}%` : 'Loading...'}
-        </p>
+        flexDirection: 'column',
+      }}>
+      <span className="canvas-loader"></span>
+      <p
+        style={{
+          fontSize: 14,
+          color: '#F1F1F1',
+          fontWeight: 800,
+          marginTop: 40,
+        }}>
+        {progress !== 0 ? `${progress.toFixed(2)}%` : 'Loading...'}
+      </p>
     </Html>
-  )
-}
+  );
+};
 
-export default CanvasLoader
+export default CanvasLoader;
