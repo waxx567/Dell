c6533d6173db554d8da556cda452226e7c028595
Wayne McRae <waynem567@gmail.com>
2024-10-16 15:31:07 +0200

    Update layout.tsx

--- portfolio/app/layout.tsx
+++ portfolio/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
           {children}
         </ThemeProvider>
       </body>
