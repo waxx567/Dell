326c499fec21c71295eba0f2a99ad8e0057df398
Wayne McRae <waynem567@gmail.com>
2024-10-17 10:07:27 +0200

    Update BentoGrid.tsx

--- portfolio/components/ui/BentoGrid.tsx
+++ portfolio/components/ui/BentoGrid.tsx
@@ -46,10 +46,12 @@ export const BentoGridItem = ({
         "row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
         className
       )}
+      style={{
+        background: 'rgb(4, 7, 29)',
+        backgroundColor: 'gradient...'
+      }}
     >
-      {header}
       <div className="group-hover/bento:translate-x-2 transition duration-200">
-        {icon}
         <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
           {title}
         </div>
