d107d79501518651cc6040e0089f60bb19be3fe3
Wayne McRae <waynem567@gmail.com>
2024-10-17 09:29:53 +0200

    Update Grid.tsx

--- portfolio/components/Grid.tsx
+++ portfolio/components/Grid.tsx
@@ -1,20 +1,28 @@
-import { BentoGrid, BentoGridItem } from './ui/BentoGrid'
+import { gridItems } from "@/data";
+import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
 
 const Grid = () => {
   return (
-    <section id='about'>
-      <BentoGrid>
-        {[{ title: 'Title1', description: 'Desc1', id: 1}].map((item) => (
+    <section id="about">
+      <BentoGrid className="w-full py-20">
+        {gridItems.map((item, i) => (
           <BentoGridItem
             id={item.id}
-            key={item.id}
+            key={i}
             title={item.title}
             description={item.description}
+            // remove icon prop
+            // remove original classname condition
+            className={item.className}
+            img={item.img}
+            imgClassName={item.imgClassName}
+            titleClassName={item.titleClassName}
+            spareImg={item.spareImg}
           />
         ))}
       </BentoGrid>
     </section>
-  )
-}
+  );
+};
 
-export default Grid
+export default Grid;
