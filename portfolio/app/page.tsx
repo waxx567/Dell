1ab9a15759b304097281e04c0f31cfd007162b27
Wayne McRae <waynem567@gmail.com>
2024-10-17 08:55:34 +0200

    Update page.tsx

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,3 +1,4 @@
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { FaHome } from "react-icons/fa";
@@ -8,6 +9,7 @@ export default function Home() {
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={[{name: 'Home', link: '/', icon: <FaHome />}]} />
         <Hero />
+        <Grid />
       </div>
     </main>  
   );
