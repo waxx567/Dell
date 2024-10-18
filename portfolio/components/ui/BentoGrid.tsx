72baca993201383ef69c9199da9662e6eea06836
Wayne McRae <waynem567@gmail.com>
2024-10-18 10:41:01 +0200

    Update BentoGrid.tsx

--- portfolio/components/ui/BentoGrid.tsx
+++ portfolio/components/ui/BentoGrid.tsx
@@ -96,7 +96,15 @@ export const BentoGridItem = ({
         {id === 3 && (
           <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
             <div className="flex flex-col gap-3 lg:gap-8">
-              {['React.js', 'Next.js', 'typescript'].map((item) => (
+              {['React.js', 'Next.js', 'Typescript'].map((item) => (
+                <span key={item} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">
+                  {item}
+                </span>
+              ))}
+              <span className="py-4 px-3 rounded-lg text-center bg-[#10132E]" />
+            </div>
+            <div className="flex flex-col gap-3 lg:gap-8">
+              {['Salesforce', 'Python', 'Flask'].map((item) => (
                 <span key={item} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">
                   {item}
                 </span>
