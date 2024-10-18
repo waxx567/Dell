6ff017257491d7da613894c53401b572494f0413
Wayne McRae <waynem567@gmail.com>
2024-10-18 10:44:02 +0200

    Update BentoGrid.tsx

--- portfolio/components/ui/BentoGrid.tsx
+++ portfolio/components/ui/BentoGrid.tsx
@@ -104,12 +104,12 @@ export const BentoGridItem = ({
               <span className="py-4 px-3 rounded-lg text-center bg-[#10132E]" />
             </div>
             <div className="flex flex-col gap-3 lg:gap-8">
+            <span className="py-4 px-3 rounded-lg text-center bg-[#10132E]" />
               {['Salesforce', 'Python', 'Flask'].map((item) => (
                 <span key={item} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">
                   {item}
                 </span>
               ))}
-              <span className="py-4 px-3 rounded-lg text-center bg-[#10132E]" />
             </div>
           </div>
         )}
