26ddf87b5a736986c2bf71a7d48764284194ff72
Wayne McRae <waynem567@gmail.com>
2024-10-18 09:54:41 +0200

    Update BentoGrid.tsx

--- portfolio/components/ui/BentoGrid.tsx
+++ portfolio/components/ui/BentoGrid.tsx
@@ -93,6 +93,18 @@ export const BentoGridItem = ({
         </div>
 
         {id === 2 && <GridGlobe />}
+
+        {id === 3 && (
+          <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
+            <div className="flex flex-col gap-3 lg:gap-8">
+              {['React.js', 'Next.js', 'typescript'].map((item) => (
+                <span>
+                  {item}
+                </span>
+              ))}
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
