826946f7013073f99339943f0bb85e83829fd64a
Wayne McRae <waynem567@gmail.com>
2024-10-18 15:32:54 +0200

    Update index.ts

--- portfolio/data/index.ts
+++ portfolio/data/index.ts
@@ -10,7 +10,7 @@ export const gridItems = [
     id: 1,
     title: "I prioritize client collaboration, fostering open communication ",
     description: "",
-    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[85vh]",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[82vh]",
     imgClassName: "w-full h-full",
     titleClassName: "justify-end",
     img: "/b1.svg",
