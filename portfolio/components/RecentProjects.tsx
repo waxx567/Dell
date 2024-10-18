d64925a5adbb87ebcb780bfbe60f6b84bafab121
Wayne McRae <waynem567@gmail.com>
2024-10-18 19:15:54 +0200

    Update RecentProjects.tsx

--- portfolio/components/RecentProjects.tsx
+++ portfolio/components/RecentProjects.tsx
@@ -11,7 +11,7 @@ const RecentProjects = () => {
       {/* Cards */}
       <div className='flex flex-wrap items-center justify-center p-4 gap-16 mt-10'>
         {projects.map(({ id, title, des, img, iconLists, link }) => (
-          <div key={id}>
+          <div key={id} className='lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[90vw]'>
             {title}
           </div>
         ))}
