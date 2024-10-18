28143329487363a6f8e753f16de5227ed87caec0
Wayne McRae <waynem567@gmail.com>
2024-10-18 14:00:34 +0200

    Update BentoGrid.tsx

--- portfolio/components/ui/BentoGrid.tsx
+++ portfolio/components/ui/BentoGrid.tsx
@@ -54,8 +54,8 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
-  const leftLists = ["ReactJS", "Express", "Typescript"];
-  const rightLists = ["VueJS", "NuxtJS", "GraphQL"];
+  const leftLists = ["ReactJS", "Typescript", "Salesforce"];
+  const rightLists = ["NextJS", "Python", "Flask"];
 
   const [copied, setCopied] = useState(false);
 
