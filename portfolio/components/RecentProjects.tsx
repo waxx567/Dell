7033ea9f9ea98774d41a5278663f9f1345a4641b
Wayne McRae <waynem567@gmail.com>
2024-10-18 15:50:19 +0200

    Update RecentProjects.tsx

--- portfolio/components/RecentProjects.tsx
+++ portfolio/components/RecentProjects.tsx
@@ -5,6 +5,7 @@ const RecentProjects = () => {
     <div className='py-20'>
       <h1 className='heading'>
         A small selection of {' '}
+        <span>recent projects</span>
       </h1>
     </div>
   )
