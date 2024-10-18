007ff79db1745a999a437baa79201c45b5f016ba
Wayne McRae <waynem567@gmail.com>
2024-10-18 08:55:18 +0200

    Update BentoGrid.tsx

--- portfolio/components/ui/BentoGrid.tsx
+++ portfolio/components/ui/BentoGrid.tsx
@@ -81,7 +81,7 @@ export const BentoGridItem = ({
         )}
 
         <div className={cn(
-          titleClassName, 'group-hover'
+          titleClassName, 'group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10'
         )}></div>
       </div>
 
