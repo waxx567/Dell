b7bcb6b18db2200188307b8782b8356d39f95482
Wayne McRae <waynem567@gmail.com>
2024-10-19 16:18:30 +0200

    Update Experience.tsx

--- portfolio/components/Experience.tsx
+++ portfolio/components/Experience.tsx
@@ -4,12 +4,12 @@ const Experience = () => {
   return (
     <div className='py-20' id="testimonials">
       <h1 className='heading'>
-        Kind words from
-        <span className='text-purple'> satisfied clients</span>
+        My
+        <span className='text-purple'> work experience</span>
       </h1>
       {/* Cards */}
       <div className='flex flex-col items-center max-lg:mt-10'>
-        
+
       </div>
     </div>
   )
