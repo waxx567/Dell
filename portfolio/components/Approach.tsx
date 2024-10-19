71aedfc537de3e15b5819789e91a074bd21332e9
Wayne McRae <waynem567@gmail.com>
2024-10-19 19:01:11 +0200

    Update Approach.tsx

--- portfolio/components/Approach.tsx
+++ portfolio/components/Approach.tsx
@@ -24,9 +24,7 @@ const Approach = () => {
         <Card 
           title="Development & Progress Update" 
           icon={<AceternityIcon order="Phase 2" />}
-          description="Once we agree on the plan, I cue my lofi playlist and dive into
-          coding. From initial sketches to polished code, I keep you updated
-          every step of the way."
+          description="Once we agree on the plan, I cue my lofi playlist and dive into coding. From initial sketches to polished code, I keep you updated every step of the way."
         >
           <CanvasRevealEffect
             animationSpeed={3}
@@ -43,9 +41,7 @@ const Approach = () => {
         <Card 
           title="Development & Launch" 
           icon={<AceternityIcon order="Phase 3" />}
-          description="This is where the magic happens! Based on the approved design, 
-          I'll translate everything into functional code, building your website
-          from the ground up."
+          description="This is where the magic happens! Based on the approved design, I'll translate everything into functional code, building your website from the ground up."
         >
           <CanvasRevealEffect
             animationSpeed={3}
