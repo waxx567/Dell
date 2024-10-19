fe67d5cf88808ead4f3bfa944036fd8a644034a3
Wayne McRae <waynem567@gmail.com>
2024-10-19 08:03:10 +0200

    Update RecentProjects.tsx

--- portfolio/components/RecentProjects.tsx
+++ portfolio/components/RecentProjects.tsx
@@ -33,7 +33,13 @@ const RecentProjects = () => {
               </p>
 
               <div className='flex items-center justify-between mt-7 mb-3'>
-                <div className='flex items-center'></div>
+                <div className='flex items-center'>
+                  {iconLists.map((icon) => (
+                    <div key={icon} className='border border-white/[0.2]'>
+                      <img src={icon} alt={icon} className='p-2' />
+                    </div>
+                  ))}
+                </div>
               </div>
             </PinContainer>
           </div>
