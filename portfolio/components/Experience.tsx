c5d8f7ec82c9884dac848e69e2b7de83134d0c03
Wayne McRae <waynem567@gmail.com>
2024-10-19 17:37:01 +0200

    Update Experience.tsx

--- portfolio/components/Experience.tsx
+++ portfolio/components/Experience.tsx
@@ -16,7 +16,7 @@ const Experience = () => {
               key={card.id}
               duration={Math.floor(Math.random() * 10000) + 10000}
               borderRadius='1.75rem'
-              className='flex-1 text-white border-neutral-200 dark:border-slate-800'
+              className='flex-1 text-white p-5  border-neutral-200 dark:border-slate-800'
             >
               <div className='flex lg:flex-row flex-col lg:items-center p-3 py-6 md:p-5 lg:p-10 gap-2'>
                 <img src={card.thumbnail} alt={card.thumbnail} className='lg:w-32 md:w-20 w-16' />
