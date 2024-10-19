13f109e51d60a3c8abe65fc8ed721b3be6b249f1
Wayne McRae <waynem567@gmail.com>
2024-10-19 19:41:17 +0200

    Update Footer.tsx

--- portfolio/components/Footer.tsx
+++ portfolio/components/Footer.tsx
@@ -15,7 +15,7 @@ const Footer = () => {
         <h1 className='heading lg:max-w-[45vw]'>
           Ready to take <span className='text-purple'>your</span> digital prescence to the next level?
         </h1>
-        <p>Reach out to me today and let&apos;s discuss how I can help you achieve your goals.</p>
+        <p className='text-white-200 md:mt-10 my-5 text-center'>Reach out to me today and let&apos;s discuss how I can help you achieve your goals.</p>
       </div>
     </footer>
   )
