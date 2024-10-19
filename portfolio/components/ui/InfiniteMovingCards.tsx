266fad195fb3bb89515f2c5d394471f719cc4a45
Wayne McRae <waynem567@gmail.com>
2024-10-19 12:25:02 +0200

    Update InfiniteMovingCards.tsx

--- portfolio/components/ui/InfiniteMovingCards.tsx
+++ portfolio/components/ui/InfiniteMovingCards.tsx
@@ -87,7 +87,7 @@ export const InfiniteMovingCards = ({
       >
         {items.map((item) => (
           <li
-            className="w-[350px] max-w-full relative rounded-2xl border border-b-0 flex-shrink-0 border-slate-700 px-8 py-6 md:w-[60vw]"
+            className="w-[90vw] max-w-full relative rounded-2xl border border-b-0 flex-shrink-0 border-slate-800 p-5 md:p-16 md:w-[60vw]"
             style={{
               background:
                 "linear-gradient(180deg, var(--slate-800), var(--slate-900)",
