a9ab78d126f24593b3fe26a1ecfc9f24ff9e4dde
Wayne McRae <waynem567@gmail.com>
2024-10-19 16:13:07 +0200

    Update page.tsx

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -2,6 +2,7 @@ import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
 import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { navItems } from "@/data";
 
@@ -14,6 +15,7 @@ export default function Home() {
         <Grid />
         <RecentProjects />
         <Clients />
+        <Experience />
       </div>
     </main>  
   );
