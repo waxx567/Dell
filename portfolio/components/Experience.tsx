dd76e54f477904b6a4e3e71143d64d9a4947f25e
Wayne McRae <waynem567@gmail.com>
2024-10-19 16:32:46 +0200

    Update Experience.tsx

--- portfolio/components/Experience.tsx
+++ portfolio/components/Experience.tsx
@@ -1,4 +1,6 @@
+import { workExperience } from '@/data'
 import React from 'react'
+import { Button } from './ui/MovingBorders'
 
 const Experience = () => {
   return (
@@ -8,8 +10,14 @@ const Experience = () => {
         <span className='text-purple'> work experience</span>
       </h1>
       
-      <div className='w-full mt-12 grid lg:grid-cols-4 grid-cols-1'>
-
+      <div className='w-full mt-12 grid lg:grid-cols-4 grid-cols-1 gap-10'>
+        {workExperience.map((card) => (
+            <Button>
+              <div>
+                <img src={card.thumbnail} alt={card.thumbnail} className='lg:w-32 md:w-20 w-16' />
+              </div>
+            </Button>
+        ))}
       </div>
     </div>
   )
