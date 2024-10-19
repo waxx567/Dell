1b0018ac58c3042170d646e44e0ea7f91048eff7
Wayne McRae <waynem567@gmail.com>
2024-10-19 18:49:53 +0200

    Update Approach.tsx

--- portfolio/components/Approach.tsx
+++ portfolio/components/Approach.tsx
@@ -13,15 +13,21 @@ const Approach = () => {
       <div className="my-20 flex flex-col lg:flex-row items-center justify-center gap-4">
         <Card 
           title="Planning & Strategy" 
-          icon={<AceternityIcon order="Phase 1" />}>
+          icon={<AceternityIcon order="Phase 1" />}
+          des="We'll collaborate to map out your website's goals, target audience, and key functionalities. We'll discuss things like site structure, navigation, and content requirements."
+        >
           <CanvasRevealEffect
             animationSpeed={5.1}
             containerClassName="bg-emerald-900"
           />
         </Card>
         <Card 
-          title="Nisha is Munni" 
-          icon={<AceternityIcon order="Phase 2" />}>
+          title="Development & Progress Update" 
+          icon={<AceternityIcon order="Phase 2" />}
+          des="Once we agree on the plan, I cue my lofi playlist and dive into
+          coding. From initial sketches to polished code, I keep you updated
+          every step of the way."
+        >
           <CanvasRevealEffect
             animationSpeed={3}
             containerClassName="bg-black"
@@ -35,8 +41,12 @@ const Approach = () => {
           <div className="absolute inset-0 [mask-image:radial-gradient(400px_at_center,white,transparent)] bg-black/50 dark:bg-black/90" />
         </Card>
         <Card 
-          title="Munni is Aditi" 
-          icon={<AceternityIcon order="Phase 3" />}>
+          title="Development & Launch" 
+          icon={<AceternityIcon order="Phase 3" />}
+          des="This is where the magic happens! Based on the approved design, 
+          I'll translate everything into functional code, building your website
+          from the ground up."
+        >
           <CanvasRevealEffect
             animationSpeed={3}
             containerClassName="bg-sky-600"
