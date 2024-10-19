79cfede5965ce35b8983fd9aa0224cee4713f313
Wayne McRae <waynem567@gmail.com>
2024-10-19 17:12:23 +0200

    Update Experience.tsx

--- portfolio/components/Experience.tsx
+++ portfolio/components/Experience.tsx
@@ -21,6 +21,14 @@ const Experience = () => {
               <div className='flex lg:flex-row flex-col lg:items-center p-3 py-6 md:p-5 lg:p-10 gap-2'>
                 <img src={card.thumbnail} alt={card.thumbnail} className='lg:w-32 md:w-20 w-16' />
               </div>
+              <div className='lg:ms-5'>
+                <h1>
+                  {card.title}
+                </h1>
+                <p>
+                  {card.desc}
+                </p>
+              </div>
             </Button>
         ))}
       </div>
