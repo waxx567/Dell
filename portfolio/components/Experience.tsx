16ab36fd8b042e3aa8646a49c815eb9acc10f274
Wayne McRae <waynem567@gmail.com>
2024-10-19 16:17:05 +0200

    Update Experience.tsx

--- portfolio/components/Experience.tsx
+++ portfolio/components/Experience.tsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Experience = () => {
   return (
-    <div>
-      Experience
+    <div className='py-20' id="testimonials">
+      <h1 className='heading'>
+        Kind words from
+        <span className='text-purple'> satisfied clients</span>
+      </h1>
+      {/* Cards */}
+      <div className='flex flex-col items-center max-lg:mt-10'>
+        
+      </div>
     </div>
   )
 }
