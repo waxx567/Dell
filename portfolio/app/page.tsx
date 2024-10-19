4f4ef91b813c5a155f64b921bd2f926ebd99773c
Wayne McRae <waynem567@gmail.com>
2024-10-19 19:20:31 +0200

    Update page.tsx

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -4,6 +4,7 @@ import RecentProjects from "@/components/RecentProjects";
 import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
 import Approach from "@/components/Approach";
+import Footer from "@/components/Footer";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { navItems } from "@/data";
 
@@ -18,6 +19,7 @@ export default function Home() {
         <Clients />
         <Experience />
         <Approach />
+        <Footer />
       </div>
     </main>  
   );
