cfd4c4d97f0ec3775e7c25215c75b10aea78a22e
Wayne McRae <waynem567@gmail.com>
2024-10-19 19:29:45 +0200

    Update Footer.tsx

--- portfolio/components/Footer.tsx
+++ portfolio/components/Footer.tsx
@@ -2,9 +2,15 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
+    <footer className='w-full pt-20 pb-10' id='contact'>
+      <div className='w-full absolute left-0 -bottom-72 min-h-96'>
+        <img 
+          src='footer-grid.svg'
+          alt='grid'
+          className='w-full h-full opacity-50'
+        />
+      </div>
+    </footer>
   )
 }
 
