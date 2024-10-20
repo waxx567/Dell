a93be774fb3273a20a40866a689450ea68a48467
Wayne McRae <waynem567@gmail.com>
2024-10-20 06:50:29 +0200

    Update index.ts

--- portfolio/data/index.ts
+++ portfolio/data/index.ts
@@ -80,7 +80,7 @@ export const projects = [
   },
   {
     id: 2,
-    title: "Yoom - Video Conferencing App",
+    title: "Python",
     des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
     img: "/python.jpg",
     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
@@ -88,8 +88,8 @@ export const projects = [
   },
   {
     id: 3,
-    title: "AI Image SaaS - Canva Application",
-    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+    title: "Salesforce",
+    des: "Salesforce is the world's #1 CRM platform that connects all your data, teams, and AI on one integrated system.",
     img: "/salesforce.png",
     iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
     link: "/ui.aiimg.com",
