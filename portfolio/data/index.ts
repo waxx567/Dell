e1faeffa50e4f5c1edfec132bdc8ad68d79f33fb
Wayne McRae <waynem567@gmail.com>
2024-10-20 13:18:46 +0200

    Update index.ts

--- portfolio/data/index.ts
+++ portfolio/data/index.ts
@@ -162,7 +162,7 @@ export const testimonials = [
   {
     quote:
       "Frontend Mentor offers a variety of coding challenges for front-end developers of different levels. I improved my front-end skills by building real projects, and solved real-world HTML, CSS and JavaScript challenges whilst working to professional designs. I built projects, learn new skills, and joined the Discord community. This was an invaluable resource for me at a time when I was just starting out and unsure of my abilities.",
-    name: "Frontend Mentor 2023",
+    name: "HTML, CSS & JavaScript 2023",
     title: "frontendmentor.io",
   },
 ];
