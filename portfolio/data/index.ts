8fdb5f29218fe90540353cc66b4738fbce2465c3
Wayne McRae <waynem567@gmail.com>
2024-10-20 12:35:53 +0200

    More unique data

--- portfolio/data/index.ts
+++ portfolio/data/index.ts
@@ -149,7 +149,7 @@ export const testimonials = [
   },
   {
     quote:
-      "WordPress is a web content management system. It was originally created as a tool to publish blogs but has evolved to support publishing other web content, including more traditional websites, mailing lists and Internet forum, media galleries, membership sites, learning management systems, and online stores. Available as free and open-source software, WordPress is among the most popular content management systems, and was used by 43.1% of the top 10 million websites in 2023.",
+      "WordPress is a web content management system. It was originally created as a tool to publish blogs but has evolved to support publishing other web content, including more traditional websites, mailing lists and Internet forum, media galleries, membership sites, learning management systems, and online stores. WordPress is  the most popular content management system in the world, and was used by 43.1% of the top 10 million websites in 2023.",
     name: "WordPress 2024",
     title: "I taught myself WordPress",
   },
