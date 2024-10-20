98a34fa8360a126dddb4697fea515fa1ac9f1113
Wayne McRae <waynem567@gmail.com>
2024-10-20 09:46:02 +0200

    Update InfiniteMovingCards.tsx

--- portfolio/components/ui/InfiniteMovingCards.tsx
+++ portfolio/components/ui/InfiniteMovingCards.tsx
@@ -105,7 +105,7 @@ export const InfiniteMovingCards = ({
               </span>
               <div className="relative z-20 mt-4 lg:mt-6 flex flex-row items-center">
                 <span className="flex flex-col gap-1">
-                  <div className="rounded-full">
+                  <div className="w-12 h-12 rounded-full">
                     <img src="/wayne.jpg" alt="profile" />
                   </div>
                   <div className="flex flex-col gap-1">
