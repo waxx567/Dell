6514264f4a3f3baa92ff528c155999c406a6a97b
Wayne McRae <waynem567@gmail.com>
2024-10-20 18:20:58 +0200

    Update index.ts

--- portfolio/data/index.ts
+++ portfolio/data/index.ts
@@ -54,7 +54,7 @@ export const gridItems = [
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
     titleClassName: "justify-center md:justify-start lg:justify-center",
-    img: "/b5.svg",
+    img: "/three.png",
     spareImg: "/grid.svg",
   },
   {
