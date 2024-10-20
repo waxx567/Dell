2ca52db3d3656466ece5114388bf4fa690419c2b
Wayne McRae <waynem567@gmail.com>
2024-10-20 18:48:48 +0200

    Update index.ts

--- portfolio/data/index.ts
+++ portfolio/data/index.ts
@@ -75,7 +75,7 @@ export const projects = [
     title: "CS50X 2023, CS50P 2023, CS50AI 2024",
     des: "CS50 is a computer science course taught at Harvard University, Yale University and Dartmouth College.",
     img: "/harvard.png",
-    iconLists: ["/harvard.svg", "/zoom.svg", "/bootstrap.svg", "/php.svg", "/github-grey.svg", "/sqlite.svg"],
+    iconLists: ["/harvard.svg", "/github-grey.svg", "/bootstrap.svg", "/php.svg", "/zoom.svg", "/sqlite.svg"],
     link: "#testimonials",
   },
   {
