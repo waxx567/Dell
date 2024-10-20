ba8146ade72a1ec24fcb99e36a1a038260b23055
Wayne McRae <waynem567@gmail.com>
2024-10-20 10:02:44 +0200

    Update index.ts

--- portfolio/data/index.ts
+++ portfolio/data/index.ts
@@ -107,7 +107,7 @@ export const projects = [
 export const testimonials = [
   {
     quote:
-      "CS50X teaches students to solve problems, both with and without code, emphasizing correctness, design, and style. Topics generally include computational thinking, abstraction, algorithms, data structures, and computer science. I learned to program in C and Python, design, query, and process SQL databases, and code in HTML, CSS, and JavaScript. I also gained an understanding of computer hardware, industry best practices, and memory allocation.",
+      "CS50X teaches students to solve problems, both with and without code, emphasizing correctness, design, and style. Topics generally include computational thinking, abstraction, algorithms, data structures, and computer science. I learned to program in C, Python, HTML, CSS, and JavaScript, design, query, and process SQL databases. I also gained an understanding of computer hardware, industry best practices, and memory allocation.",
     name: "CS50X Certificate 2023",
     title: "Harvard University",
   },
@@ -119,7 +119,7 @@ export const testimonials = [
   },
   {
     quote:
-      "CS50AI explores the concepts and algorithms at the foundation of modern artificial intelligence, diving into the ideas that give rise to technologies like game-playing engines, handwriting recognition, and machine translation. Through hands-on projects, students gain exposure to the theory behind graph search algorithms, classification, optimization, machine learning, large language models, and other topics in artificial intelligence as they incorporate them into their own Python programs. Students emerge with experience in libraries for machine learning as well as knowledge of AI principles to design intelligent systems of their own.",
+      "CS50AI explores the concepts and algorithms at the foundation of modern artificial intelligence, diving into the ideas that give rise to technologies like game-playing engines, handwriting recognition, machine translation, graph search algorithms, classification, optimization, machine learning, large language models. Students emerge with experience in libraries for machine learning as well as knowledge of AI principles to design intelligent systems of their own.",
     name: "CS50AI Certificate 2023",
     title: "Harvard University",
   },
