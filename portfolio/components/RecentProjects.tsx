aac96844719f2a0ef1eeb3a693036cc907ee3a04
Wayne McRae <waynem567@gmail.com>
2024-10-20 09:01:05 +0200

    Update RecentProjects.tsx

--- portfolio/components/RecentProjects.tsx
+++ portfolio/components/RecentProjects.tsx
@@ -16,14 +16,14 @@ const RecentProjects = () => {
         {projects.map(({ id, title, des, img, iconLists, link }) => (
           <div key={id} className='sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-[570px] w-[80vw]'>
             <PinContainer title={link} href={link}>
-              <div className='relative flex items-center justify-top sm:w-[570px] w-[80vw] overflow-hidden sm:h-[40vh] h-[30vh] mb-10'>
+              <div className='relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h-[40vh] h-[30vh] mb-10'>
                 <div>
                   <img src='/bg.png' alt='bg-img' />
                 </div>
                 <img 
                   src={img}
                   alt={title}
-                  className='z-10 absolute bottom-0'
+                  className='z-10 absolute -top-5'
                 />
               </div>
               <h1 className='font-bold lg:text-2xl md:text-xl text-base line-clamp-1'>
