6ce981f7e9cb9dcbc7fb3e71f6a8a9fb03fe3717
Wayne McRae <waynem567@gmail.com>
2024-10-20 06:41:01 +0200

    Update index.ts

--- portfolio/data/index.ts
+++ portfolio/data/index.ts
@@ -82,7 +82,7 @@ export const projects = [
     id: 2,
     title: "Yoom - Video Conferencing App",
     des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
-    img: "/python.png",
+    img: "/python.jpg",
     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
     link: "/ui.yoom.com",
   },
