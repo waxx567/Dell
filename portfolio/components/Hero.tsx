af688c1c816396dad8423f7a8c9fe868c621d259
Wayne McRae <waynem567@gmail.com>
2024-10-21 05:19:24 +0200

    Update Hero.tsx

--- portfolio/components/Hero.tsx
+++ portfolio/components/Hero.tsx
@@ -57,7 +57,7 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Wayne, a Junior Developer based in South Africa.
+            Hi! I&apos;m Wayne, a Junior Developer from South Africa.
           </p>
 
           <a href="#about">
