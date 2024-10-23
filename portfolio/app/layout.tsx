9071fe4f919acd94ee8a1e748f0bcc73598d0bd9
Wayne McRae <waynem567@gmail.com>
2024-10-23 11:10:45 +0200

    Change site icon

--- portfolio/app/layout.tsx
+++ portfolio/app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "./provider";
 
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
