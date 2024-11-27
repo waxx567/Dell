cc6ecd40caef76757216c4beeffbee0451bbfab6
Wayne McRae <waynem567@gmail.com>
2024-11-27 11:56:40 +0200

    Update helloworld.cpp

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -1,7 +1,10 @@
-#include <iostream> // iostream is a header file that contains functions for basic input/output operations
+#include <iostream> 
 
+// Introducing variables
 int main() {
-    std::cout << "I like cake!" << '\n';
-    std::cout << "It's really good!" << std::endl; // endl is an endline character. Does the same as \n but also flushes the output buffer
+    
+    int x; // Declaring a variable
+    x = 5; // Assigning a value to a variable
+
     return 0;
 }
