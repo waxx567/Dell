8353e0dffb35292b280cee60b0c65d79d570306b
Wayne McRae <waynem567@gmail.com>
2024-11-27 13:06:24 +0200

    Update helloworld.cpp

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -2,19 +2,10 @@
 
 int main() {
     
-    // The const keyword specifies that a variable's value is constant
-    // tells the compiler to prevent anything from modifying it
-    // (read-only)
-
-    // Create a program to calculate the circumference of a circle
-    const double PI = 3.14159;
-    PI = 420.69;
-    double radius = 10;
-    double circumference = 2 * PI * radius;
-
-    std::cout << circumference << "cm";
-
-    // Adding const will throw an error because the value of PI is constant
+    // Namespace provides a solution for preventing name conflicts
+    // in large projects. Each entity needs a unique name.
+    // A namespace allows for identically named entities 
+    // as long as the namespaces are different
 
     return 0;
 }
