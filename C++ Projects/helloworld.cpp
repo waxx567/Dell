dbb30283930aa3d07c882081d73efaa56ced0338
Wayne McRae <waynem567@gmail.com>
2024-11-27 12:48:00 +0200

    Update helloworld.cpp

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -7,13 +7,14 @@ int main() {
     // (read-only)
 
     // Create a program to calculate the circumference of a circle
-    double pi = 3.14159;
-    // All good but if someone changes the value of pi it will change the result of the program
-    pi = 420.69;
+    const double PI = 3.14159;
+    PI = 420.69;
     double radius = 10;
-    double circumference = 2 * pi * radius;
+    double circumference = 2 * PI * radius;
 
     std::cout << circumference << "cm";
+
+    // Adding const will throw an error because the value of PI is constant
 
     return 0;
 }
