28dd569ad670832c722238cd6f261e59c44cb5b2
Wayne McRae <waynem567@gmail.com>
2024-11-27 15:25:57 +0200

    C++ continued

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -1,13 +1,19 @@
 #include <iostream> 
+#include <vector>
+
+typedef std::vector<std::pair<std::string, int>> pairlist_t;
 
 int main() {
     
-    // typedef = reserved keyword used tocreate an additional name
+    // typedef = reserved keyword used to create an additional name
     //           (alias) for another data type.
     //           New identifier for an existing data type.
     //           Helps with readability and reduces typos.
 
-    
+    // so
+    // std::vector<std::pair<std::string, int>> pairlist;
+    // becomes
+    pairlist_t pairlist;
 
     return 0;
 }
