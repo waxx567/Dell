caff5138c9112015d38e3cde37717b1f60bbafd4
Wayne McRae <waynem567@gmail.com>
2024-11-27 13:11:49 +0200

    Update helloworld.cpp

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -1,11 +1,24 @@
 #include <iostream> 
 
+namespace first {
+    int x = 1;
+}
+namespace second {
+    int x = 2;
+}
+
 int main() {
     
     // Namespace provides a solution for preventing name conflicts
     // in large projects. Each entity needs a unique name.
     // A namespace allows for identically named entities 
-    // as long as the namespaces are different
+    // as long as the namespaces are different.
+
+    int x = 0;
+
+    std::cout << x; // outputs 0
+    std::cout << first::x; // outputs 1
+    std::cout << second::x; // outputs 2
 
     return 0;
 }
