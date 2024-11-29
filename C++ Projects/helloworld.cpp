934cdf3804555b896baea3aaa78b5d4ce6d4f0d2
Wayne McRae <waynem567@gmail.com>
2024-11-29 07:51:25 +0200

    Update helloworld.cpp

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -6,9 +6,16 @@ int main() {
     // cout << (insertion operator)
     // cin >> (extraction operator)
     std::string name;
+    int age;
+
     std::cout << "Enter your name: ";
     std::cin >> name;
+
+    std::cout << "How old are you? ";
+    std::cin >> age;
+
     std::cout << "Hello, " << name << "!" << std::endl;
+    std::cout << "You are " << age << " years old." << std::endl;
 
     return 0;
 }
