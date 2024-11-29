74213b881b44e109ed8e51545c1c7e09f7710161
Wayne McRae <waynem567@gmail.com>
2024-11-29 07:39:35 +0200

    Update helloworld.cpp

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -7,12 +7,10 @@ int main() {
 
     int correct = 8;
     int questions = 10;
-    double percent = correct / questions * 100;
+    // so you can explicitly cast questions as a double
+    double percent = correct / (double) questions * 100;
 
-    std::cout << percent << "%"; // outputs 0% because the int data type truncates the value
-
-    // this won't work because the output is a double
-    // std::cout << correct / questions * 100 << "%";
+    std::cout << percent << "%"; // outputs 80%
 
     return 0;
 }
