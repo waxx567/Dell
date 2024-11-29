daad9c1233c26aa88b32219b11e5335501b0652a
Wayne McRae <waynem567@gmail.com>
2024-11-29 07:43:45 +0200

    Update helloworld.cpp

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -2,15 +2,13 @@
 
 int main() {
     
-    // calculate how many questions a student gets right
-    // out of the total number of questions
-
-    int correct = 8;
-    int questions = 10;
-    // so you can explicitly cast questions as a double
-    double percent = correct / (double) questions * 100;
-
-    std::cout << percent << "%"; // outputs 80%
+    // accepting user input
+    // cout << (insertion operator)
+    // cin >> (extraction operator)
+    std::string name;
+    std::cout << "Enter your name: ";
+    std::cin >> name;
+    std::cout << "Hello, " << name << "!" << std::endl;
 
     return 0;
 }
