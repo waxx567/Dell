297e5c3ca6a22e1405933ad05708ae09423871cf
Wayne McRae <waynem567@gmail.com>
2024-11-29 11:54:20 +0200

    Update helloworld.cpp

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -3,16 +3,24 @@
 
 int main() {
     
-    double x = 3.14;
-    double y = 4;
-    double z;
-    
-    // z = round(x); // z = 3
-    // z = ceil(x); // z = 4
-    // z = floor(x); // z = 3
-    // z = trunc(x); // z = 3
+    // calculate the hypotenuse of a right triangle
+    double a;
+    double b;
+    double c;
+    // solution: c is the square root of a squared + b squared
 
-    std::cout << z;
+    std::cout << "Enter the length of side a: ";
+    std::cin >> a;
+
+    std::cout << "Enter the length of side b: ";
+    std::cin >> b;
+
+    a = pow(a, 2);
+    b = pow(b, 2);
+    c = sqrt(a + b);
+
+    std::cout << "The length of the hypotenuse is: " << c << std::endl;
+
 
     return 0;
 }
