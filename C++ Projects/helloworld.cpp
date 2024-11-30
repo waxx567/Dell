5916c8cf6af19239c5ff23defa8135d0405d64a4
Wayne McRae <waynem567@gmail.com>
2024-11-30 16:50:22 +0200

    Update helloworld.cpp

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -5,9 +5,10 @@ int main() {
     // for loop = executes a block of code a specified number of times
     //            can use a counter variable to keep track of how many times the loop has run 
 
-    for (int i = 1; i <= 20; i+=2) {
+    for (int i = 10; i >= 1; i--) {
         std::cout << i << std::endl;
     }
+    std::cout << "HAPPY NEW YEAR!" << std::endl;
 
     return 0;
 }
