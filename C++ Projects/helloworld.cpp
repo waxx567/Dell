9149f6bc2221788151e78dbb14414ca99a32d1ac
Wayne McRae <waynem567@gmail.com>
2024-11-30 14:57:20 +0200

    Update helloworld.cpp

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -2,28 +2,25 @@
 
 int main() {
 
-    // create a temperature converter
+    // useful string methods
+    std::string name;
+    
+    std::cout << "Enter your name: ";
+    std::getline(std::cin, name);
 
-    double temp;
-    char unit;
+    // name.length(); // returns the number of characters in the string
+    // name.empty(); // returns true if the string is empty, false if not
+    // name.find(" "); // returns the index of the first space in the string
 
-    std::cout << "Enter the temperature: ";
-    std::cin >> temp;
-
-    std::cout << "Enter the unit: ";
-    std::cin >> unit;
-
-    if (unit == 'C' || unit == 'c') {
-        temp = temp * 1.8 + 32;
-        std::cout << temp << "F";
+    if (name.length() > 16)
+    {
+        std::cout << "Your name is too long" << std::endl;
     }
-    else if (unit == 'F' || unit == 'f') {    
-        temp = (temp - 32) / 1.8;
-        std::cout << temp << "C";
+    else
+    {
+        std::cout << "Your name is " << name.length() << " characters long" << std::endl;
     }
-    else {
-        std::cout << "Invalid unit";
-    }
+    
     
     return 0;
 }
