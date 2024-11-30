8e030fac0e7975b7aa7a2b7f47e43db7a37c2b00
Wayne McRae <waynem567@gmail.com>
2024-11-30 15:21:51 +0200

    Update helloworld.cpp

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -9,7 +9,7 @@ int main() {
     std::getline(std::cin, name);
 
     // name.insert(0, "Hello, "); // insert a string at a specific index
-    name.erase(0, 5); // erase a string at a specific index
+    // name.erase(0, 5); // erase a string at a specific index
     std::cout << name << std::endl;
 
     return 0;
