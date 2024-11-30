73e38b9bf76a044e26dd0d865d2ce43a77897cd9
Wayne McRae <waynem567@gmail.com>
2024-11-30 07:55:57 +0200

    Update helloworld.cpp

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -13,5 +13,44 @@ int main() {
     std::cout << "Enter the number of the month: ";
     std::cin >> month;
 
+    switch (month) {
+        case 1:
+            std::cout << "It is January" << std::endl;
+            break;
+        case 2:
+            std::cout << "It is February" << std::endl;
+            break;
+        case 3:
+            std::cout << "It is March" << std::endl;
+            break; 
+        case 4:
+            std::cout << "It is April" << std::endl;
+            break;
+        case 5:
+            std::cout << "It is May" << std::endl;
+            break;
+        case 6:
+            std::cout << "It is June" << std::endl;
+            break;
+        case 7:
+            std::cout << "It is July" << std::endl;
+            break;
+        case 8:
+            std::cout << "It is August" << std::endl;
+            break;
+        case 9:
+            std::cout << "It is September" << std::endl;
+            break;
+        case 10:
+            std::cout << "It is October" << std::endl;
+            break;
+        case 11:
+            std::cout << "It is November" << std::endl;
+            break;
+        case 12:
+            std::cout << "It is December" << std::endl;
+            break;
+    }
+
     return 0;
 }
