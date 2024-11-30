2fd869a09c723d13a61b25678834a293434de7f3
Wayne McRae <waynem567@gmail.com>
2024-11-30 15:11:33 +0200

    Update helloworld.cpp

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -8,19 +8,11 @@ int main() {
     std::cout << "Enter your name: ";
     std::getline(std::cin, name);
 
-    // name.length(); // returns the number of characters in the string
-    // name.empty(); // returns true if the string is empty, false if not
-    // name.find(" "); // returns the index of the first space in the string
-    // name.clear(); // clears the string
+    //name.append(" "); // adds a space to the end of the string
 
-    std::string name;
-    
-    std::cout << "Enter your name: ";
-    std::getline(std::cin, name);
+    name.append("@gmail.com");
 
-    name.clear();
-
-    std::cout << "Hello " << name << "!" << std::endl;
+    std::cout << "Your email is: " << name << std::endl;
 
     return 0;
 }
