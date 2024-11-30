e97b4a59ac8049aab74d496b37f2b0b39a573e82
Wayne McRae <waynem567@gmail.com>
2024-11-30 19:24:14 +0200

    Update helloworld.cpp

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -1,31 +1,9 @@
 #include <iostream>
+#include <ctime>
 
 int main() {
 
-    // nested loops = a loop within a loop
-    // outer loop = the loop that contains the inner loop
-    // inner loop = the loop that is contained within the outer loop
-
-    // print a rectangle with user input
-    int rows;
-    int columns;
-    char symbol;
-
-    std::cout << "Enter the number of rows: ";
-    std::cin >> rows;
-
-    std::cout << "Enter the number of columns: ";
-    std::cin >> columns;
-
-    std::cout << "Enter the symbol: ";
-    std::cin >> symbol;
-
-    for (int i = 1; i <= rows; i++) {
-        for (int j = 1; j <= columns; j++) {
-            std::cout << symbol;
-        }
-        std::cout << std::endl;
-    }
+    // pseudo-random number generator 
 
     return 0;
 }
