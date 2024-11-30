d0665dbb550a1aa71fcb3208fa80935792f8819e
Wayne McRae <waynem567@gmail.com>
2024-11-30 08:03:16 +0200

    Update helloworld.cpp

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -3,57 +3,31 @@
 
 int main() {
     
-    // switch = alternative to using multiple if statements
-    //          allows for multiple conditions to be tested
-    //          instead of testing one condition at a time
-    //          if (month == 1) { ... } else if (month == 2) { ... } etc. would be redundant in this case
+    // switch = another example
 
-    // switch is similar to a series of if statements
+    char grade;
 
-    int month;
+    std::cout << "Enter your grade: ";
+    std::cin >> grade;
 
-    std::cout << "Enter the number of the month: ";
-    std::cin >> month;
-
-    switch (month) {
-        case 1:
-            std::cout << "It is January" << std::endl;
+    switch (grade) {
+        case 'A':
+            std::cout << "You got an A!" << std::endl;
             break;
-        case 2:
-            std::cout << "It is February" << std::endl;
+        case 'B':
+            std::cout << "You got a B!" << std::endl;
             break;
-        case 3:
-            std::cout << "It is March" << std::endl;
-            break; 
-        case 4:
-            std::cout << "It is April" << std::endl;
+        case 'C':
+            std::cout << "You got a C!" << std::endl;
             break;
-        case 5:
-            std::cout << "It is May" << std::endl;
+        case 'D':
+            std::cout << "You got a D!" << std::endl;
             break;
-        case 6:
-            std::cout << "It is June" << std::endl;
-            break;
-        case 7:
-            std::cout << "It is July" << std::endl;
-            break;
-        case 8:
-            std::cout << "It is August" << std::endl;
-            break;
-        case 9:
-            std::cout << "It is September" << std::endl;
-            break;
-        case 10:
-            std::cout << "It is October" << std::endl;
-            break;
-        case 11:
-            std::cout << "It is November" << std::endl;
-            break;
-        case 12:
-            std::cout << "It is December" << std::endl;
+        case 'F':
+            std::cout << "You got an F!" << std::endl;
             break;
         default:
-            std::cout << "Invalid month number" << std::endl;
+            std::cout << "Invalid grade!" << std::endl;
             break;
     }
 
