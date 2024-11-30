99b3e42ff23238fcada0d56dda4eb9b7ff94344a
Wayne McRae <waynem567@gmail.com>
2024-11-30 19:17:13 +0200

    Update helloworld.cpp

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -6,9 +6,23 @@ int main() {
     // outer loop = the loop that contains the inner loop
     // inner loop = the loop that is contained within the outer loop
 
-    for (int i = 1; i <= 3; i++) {
-        for (int j = 1; j <= 10; j++) {
-            std::cout << j << ' ';
+    // print a rectangle with user input
+    int rows;
+    int columns;
+    char symbol;
+
+    std::cout << "Enter the number of rows: ";
+    std::cin >> rows;
+
+    std::cout << "Enter the number of columns: ";
+    std::cin >> columns;
+
+    std::cout << "Enter the symbol: ";
+    std::cin >> symbol;
+
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= columns; j++) {
+            std::cout << symbol;
         }
         std::cout << std::endl;
     }
