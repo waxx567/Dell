9b8f5f01378dd37391b568742e85f84acf04ee3f
Wayne McRae <waynem567@gmail.com>
2024-11-30 10:14:21 +0200

    Update helloworld.cpp

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -5,11 +5,11 @@ int main() {
     // ternary operator ?: = replacement to an if/else statement
     // condition ? expression1 : expression2
 
-    int grade = 75;
+    // int grade = 75;
+    // grade >= 60 ? std::cout << "You passed!" : std::cout << "You failed!";
 
-    // do this
-
-    grade >= 60 ? std::cout << "You passed!" : std::cout << "You failed!";
+    int number = 9;
+    number % 2 == 0 ? "Number is even" : "Number is odd";
     
     return 0;
 }
