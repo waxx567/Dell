6970742caecb6bd80a559147708f5e5d182461bb
Wayne McRae <waynem567@gmail.com>
2024-11-30 17:10:53 +0200

    Update helloworld.cpp

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -6,6 +6,10 @@ int main() {
     // continue = used to skip an iteration of a loop
 
     for (int i = 1; i <= 20; i++) {
+        if (i == 13)
+        {
+            break;
+        }
         
         std::cout << i << std::endl;
     }
