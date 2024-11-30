537a69ff323c17f34b03d28a207fd1f038bf1b9b
Wayne McRae <waynem567@gmail.com>
2024-11-30 19:04:10 +0200

    Update helloworld.cpp

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -8,7 +8,7 @@ int main() {
 
     for (int i = 1; i <= 3; i++) {
         for (int j = 1; j <= 3; j++) {
-            std::cout << i << " " << j << std::endl;
+            std::cout << i << " " << j;
         }
     }
 
