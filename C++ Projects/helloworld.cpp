99c33ac57af3315cb18e76eda47837c577ccbcee
Wayne McRae <waynem567@gmail.com>
2024-12-01 17:21:06 +0200

    Update helloworld.cpp

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -1,27 +1,14 @@
 #include <iostream>
 
-/**
- * @brief Main entry point of the program
- *
- * This function initializes an array of car brands and outputs each brand
- * to the console. The array contains four elements, each representing a
- * different car brand. It then prints each brand on a new line.
- *
- * @return 0 on success
- */
 int main() {
+    
+    double prices[] = {1.99, 2.99, 3.99, 4.99, 5.99};
 
-    std::string cars[4];
-
-    cars[0] = "Lexus";
-    cars[1] = "Mazda";
-    cars[2] = "Honda";
-    cars[3] = "Ford";
-
-    std::cout << cars[0] << std::endl;
-    std::cout << cars[1] << std::endl;
-    std::cout << cars[2] << std::endl;
-    std::cout << cars[3] << std::endl;
+    std::cout << prices[0] << std::endl;
+    std::cout << prices[1] << std::endl;
+    std::cout << prices[2] << std::endl;
+    std::cout << prices[3] << std::endl;
+    std::cout << prices[4] << std::endl;
 
     return 0;
 }
