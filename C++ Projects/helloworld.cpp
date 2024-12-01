cae5eb4de59bb2780da57f319e0ac7efbcea808d
Wayne McRae <waynem567@gmail.com>
2024-12-01 17:21:48 +0200

    Update helloworld.cpp

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 
+/**
+ * @brief Main entry point of the program
+ *
+ * This function initializes an array of prices and outputs each price to the console.
+ * The array contains five elements, each representing a different price. The prices
+ * are iterated and printed using standard output.
+ *
+ * @return 0 on successful execution
+ */
 int main() {
     
     double prices[] = {1.99, 2.99, 3.99, 4.99, 5.99};
