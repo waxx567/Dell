81a96c29827ab16558c14f7764a01824a6ec892c
Wayne McRae <waynem567@gmail.com>
2024-12-01 18:50:57 +0200

    Update helloworld.cpp

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 
+/**
+ * @brief Main entry point of the program
+ *
+ * This function initializes an array of car brands and outputs each brand
+ * to the console. The array contains four elements, each representing a
+ * different brand. The brands are iterated and printed using standard output.
+ *
+ * @return 0 on successful execution
+ */
 int main() {
 
-    // iterate over elements in an array
     std::string cars[4] = {"Lexus", "Mazda", "Honda", "Ford"};
 
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < sizeof(cars)/sizeof(std::string); i++) {
         std::cout << cars[i] << std::endl;
     }
 
