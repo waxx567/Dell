c18cdbc84721cd205c111225226ec768733592f1
Wayne McRae <waynem567@gmail.com>
2024-12-01 14:09:01 +0200

    Update helloworld.cpp

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -1,105 +1,46 @@
 #include <iostream>
-#include <iomanip>
 
-void showBalance(double balance);
-double deposit();
-double withdraw(double balance);
+char getUserChoice();
+char getComputerChoice();
+void showChoice(char choice);
+void chooseWinner(char player, char computer);
 
 /**
- * @brief Main entry point of the program
+ * Main entry point of the Rock Paper Scissors game.
  *
- * This function creates a bank account program. It asks the user to make a choice
- * from the menu and performs the chosen action. The user can deposit money, withdraw
- * money, show the balance or exit the program.
- *
- * @return 0 on success
+ * This function prompts the user for input, generates a random choice
+ * for the computer, shows the choices and determines the winner.
  */
 int main() {
+    char player;
+    char computer;
 
-    double balance = 0;
-    int choice = 0;
-
-    do
-    {
-    std::cout << "Enter your choice: " << std::endl;
-    std::cout << "1. Deposit" << std::endl;
-    std::cout << "2. Withdraw" << std::endl;
-    std::cout << "3. Show balance" << std::endl;
-    std::cout << "4. Exit" << std::endl;
-
-    std::cout << "Choice: ";
-    std::cin >> choice;
-
-    if (choice != 1 || choice != 2 || choice != 3 || choice != 4)
-    {
-        std::cout << "Invalid choice" << std::endl;
-        break;
-    }
-
-    switch (choice) {
-        case 1:
-            balance += deposit();
-            showBalance(balance);
-            break;
-        case 2:
-            balance -= withdraw(balance);
-            showBalance(balance);
-            break;
-        case 3:
-            showBalance(balance);
-            break;
-        case 4:
-            std::cout << "Goodbye!" << std::endl;
-            break;
-        default:
-            std::cout << "Invalid choice" << std::endl;
-            break;
-        }
-    } while (choice != 4);
+    player = getUserChoice();
+    computer = getComputerChoice();
+    showChoice(player);
+    showChoice(computer);
+    chooseWinner(player, computer);
 
     return 0;
 }
 
-// Displays the current balance to the user
-void showBalance(double balance) {
-    std::cout << "Your balance is ZAR " << std::setprecision(2) << std::fixed << balance << std::endl;
+
+char getUserChoice() {
+    char player;
+    std::cout << "****Rock Paper Scissors Game****" << std::endl;
+    std::cout << "'r' for rock, 'p' for paper, 's' for scissors" << std::endl;    
+    std::cin >> player;
+
+    std::cout << "You choose: " << player << std::endl;
+    
+    return 0;
 }
-
-// Prompts the user to enter an amount to deposit and returns that amount.
-double deposit() {
-    double amount;
-    std::cout << "Enter the amount to deposit: ";
-    std::cin >> amount;
-
-    if (amount > 0)
-    {
-        return amount;
-    }
-    else
-    {
-        std::cout << "Invalid amount" << std::endl;
-        return 0;
-    }
+char getComputerChoice() {
+    return 0;
 }
-
-// Prompts the user to enter an amount to withdraw, checks if the amount is less than or equal to the current balance, and returns the withdrawn amount. If the amount is greater than the balance, it displays an error message and returns 0.
-double withdraw(double balance) {
-    double amount;
-    std::cout << "Enter the amount to withdraw: ";
-    std::cin >> amount;
-
-    if (amount > balance)
-    {
-        std::cout << "Insufficient funds" << std::endl;
-        return 0;
-    }
-    else if (amount > 0)
-    {
-        return amount;
-    }
-    else
-    {
-        std::cout << "Invalid amount" << std::endl;
-        return 0;
-    }
+void showChoice(char choice) {
+    return;
+}
+void chooseWinner(char player, char computer) {
+    return;
 }
