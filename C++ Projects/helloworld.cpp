a9293fecf9b172a0a2fbd2f4c1f4f213f859c3dc
Wayne McRae <waynem567@gmail.com>
2024-12-01 18:20:28 +0200

    Update helloworld.cpp

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -3,8 +3,10 @@
 /**
  * @brief Main entry point of the program
  *
- * This function initializes an array of grade characters and outputs the size
- * of the array in bytes using the sizeof operator.
+ * This function initializes an array of grade characters and outputs the
+ * number of elements in the array. The size of the array is determined by
+ * dividing the total byte size of the array by the byte size of a single 
+ * char element.
  *
  * @return 0 on successful execution
  */
@@ -12,7 +14,7 @@ int main() {
 
     char grades[] = {'A', 'B', 'C', 'D', 'F'};
 
-    std::cout << sizeof(grades) << " bytes" << std::endl;
+    std::cout << sizeof(grades)/sizeof(char) << " elements" << std::endl;
 
     return 0;
 }
