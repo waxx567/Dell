cfc876a1f435784569eea6f746b8f1f62a4a7912
Wayne McRae <waynem567@gmail.com>
2024-12-01 17:25:19 +0200

    Update helloworld.cpp

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -11,7 +11,13 @@
  */
 int main() {
     
-    double prices[] = {1.99, 2.99, 3.99, 4.99, 5.99};
+    double prices[5];
+
+    prices[0] = 10.99;
+    prices[1] = 12.50;
+    prices[2] = 9.99;
+    prices[3] = 7.50;
+    prices[4] = 6.00;
 
     std::cout << prices[0] << std::endl;
     std::cout << prices[1] << std::endl;
