b5dbc79345346d934c3372f98820eba5a18bbe9c
Wayne McRae <waynem567@gmail.com>
2024-12-01 18:46:47 +0200

    Update helloworld.cpp

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -1,20 +1,13 @@
 #include <iostream>
 
-/**
- * @brief Main entry point of the program
- *
- * This function initializes an array of car brands and outputs the number
- * of elements in the array. The size of the array is determined by dividing
- * the total byte size of the array by the byte size of a single std::string
- * element.
- *
- * @return 0 on successful execution
- */
 int main() {
 
-    std::string cars[] = {"Lexus", "Mazda", "Honda", "Ford"};
+    // iterate over elements in an array
+    std::string cars[4] = {"Lexus", "Mazda", "Honda", "Ford"};
 
-    std::cout << sizeof(cars)/sizeof(std::string) << " elements" << std::endl;
+    for (int i = 0; i < 4; i++) {
+        std::cout << cars[i] << std::endl;
+    }
 
     return 0;
 }
