6a59067c38904c7add1d63123d5624c4a146578b
Wayne McRae <waynem567@gmail.com>
2024-12-01 10:29:53 +0200

    Update helloworld.cpp

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -1,25 +1,13 @@
 #include <iostream>
 
-void bakePizza();
-void bakePizza(std::string topping1);
-void bakePizza(std::string topping1, std::string topping2);
-
 int main() {
 
-    // overloaded functions = functions with the same name but different parameters
-    //                        or different return types
+    // local variables = variables that are defined inside a function or block of code
+    // global variables = variables that are defined outside of a function or block of code and can be accessed from anywhere in the program
 
-    bakePizza("pepperoni", "mushroom");
+    int myNum = 1;
+
+    std::cout << myNum << std::endl;
 
     return 0;
-}
-
-void bakePizza() {
-    std::cout << "Here is your pizza!" << std::endl;
-}
-void bakePizza(std::string topping1) {
-    std::cout << "Here is your " << topping1 << " pizza!" << std::endl;
-} 
-void bakePizza(std::string topping1, std::string topping2) {
-    std::cout << "Here is your " << topping1 << " and " << topping2 << " pizza!" << std::endl;
 }
