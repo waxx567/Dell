2ca18d40d97106b4d5d7ec5ea2f2f7541d125073
Wayne McRae <waynem567@gmail.com>
2024-12-01 05:53:10 +0200

    Update helloworld.cpp

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -6,7 +6,7 @@ int main() {
     // pseudo-random number generator 
     srand(time(NULL)); // seed the random number generator with the current time
 
-    int num = rand(); // generate a random number between 0 and 32767
+    int num = rand() % 6; // generate a random number between 0 and 5 
 
     std::cout << num << std::endl;
 
