5bdabe2ca485d4178554f1124191b97d3c559b3f
Wayne McRae <waynem567@gmail.com>
2024-12-01 09:20:21 +0200

    Update helloworld.cpp

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 double square(double length);
+double cube(double length);
 
 int main() {
 
@@ -10,12 +11,18 @@ int main() {
 
     double length = 5.0;
     double area = square(length);
+    double volume = cube(length);
 
     std::cout << "The area of the square is: " << area << "cm^2" << std::endl;
+    std::cout << "The volume of the cube is: " << volume << "cm^3" << std::endl;
     
     return 0;
 }
 
 double square(double length) {
     return length * length;
+}
+
+double cube(double length) {
+    return length * length * length;
 }
