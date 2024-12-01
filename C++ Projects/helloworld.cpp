1d5be2ff0605d342fe14084cbe010497aa94e4b9
Wayne McRae <waynem567@gmail.com>
2024-12-01 08:31:39 +0200

    Update helloworld.cpp

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -1,23 +1,25 @@
 #include <iostream>
 
-void happyBirthday(std::string name);
+void happyBirthday(std::string name, int age);
 int main() {
 
     // function = a block of reuseable code that performs a specific task
 
     std::string name;
+    int age = 21;
     
     std::cout << "Enter your name: ";
     std::getline(std::cin, name);
 
-    happyBirthday(name);
+    happyBirthday(name, age);
     
     return 0;
 }
 
-void happyBirthday(std::string birthdayBoi) {
+void happyBirthday(std::string name, int age) {
     std::cout << "Happy birthday to you!" << std::endl;
     std::cout << "Happy birthday to you!" << std::endl;
-    std::cout << "Happy birthday, dear " << birthdayBoi << "!" << std::endl;
+    std::cout << "Happy birthday, dear " << name << "!" << std::endl;
     std::cout << "Happy birthday to you!" << std::endl;
+    std::cout << "You are " << age << " years old today." << std::endl;
 }
