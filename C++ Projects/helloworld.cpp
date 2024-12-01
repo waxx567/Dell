e0a4a1caf08c75d111295fdc6a6211e9d008d515
Wayne McRae <waynem567@gmail.com>
2024-12-01 10:49:19 +0200

    Update helloworld.cpp

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -1,19 +1,15 @@
 #include <iostream>
 
+int myNum = 3;
+
 void printNum();
 
 int main() {
-
-    // local variables = variables that are defined inside a function or block of code
-    // global variables = variables that are defined outside of a function or block of code and can be accessed from anywhere in the program
-
-    int myNum = 1;
 
     printNum();
 
     return 0;
 }
 void printNum() {
-    int myNum = 2;
     std::cout << myNum << std::endl;
 }
