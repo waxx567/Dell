cba2f35fab1001ff05d46c3507fdd6f1957ec22e
Wayne McRae <waynem567@gmail.com>
2024-12-01 09:54:59 +0200

    Update helloworld.cpp

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -2,13 +2,14 @@
 
 void bakePizza();
 void bakePizza(std::string topping1);
+void bakePizza(std::string topping1, std::string topping2);
 
 int main() {
 
     // overloaded functions = functions with the same name but different parameters
     //                        or different return types
 
-    bakePizza("pepperoni");
+    bakePizza("pepperoni", "mushroom");
 
     return 0;
 }
@@ -19,3 +20,6 @@ void bakePizza() {
 void bakePizza(std::string topping1) {
     std::cout << "Here is your " << topping1 << " pizza!" << std::endl;
 } 
+void bakePizza(std::string topping1, std::string topping2) {
+    std::cout << "Here is your " << topping1 << " and " << topping2 << " pizza!" << std::endl;
+}
