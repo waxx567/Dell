943301b45cafb9beb567d6e831034fd810a933d2
Wayne McRae <waynem567@gmail.com>
2024-12-01 06:20:28 +0200

    Update helloworld.cpp

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -3,12 +3,16 @@
 
 int main() {
 
-    // pseudo-random number generator (dice roll)
+    // pseudo-random number generator (3 dice roll)
     srand(time(NULL)); // seed the random number generator with the current time
 
-    int num = rand() % 6 + 1; // generate a random number between 1 and 6
+    int num1 = rand() % 6 + 1; // generate a random number between 1 and 6
+    int num2 = rand() % 6 + 1; // generate a random number between 1 and 6
+    int num3 = rand() % 6 + 1; // generate a random number between 1 and 6
 
-    std::cout << num << std::endl;
+    std::cout << num1 << std::endl;
+    std::cout << num2 << std::endl;
+    std::cout << num3 << std::endl;
 
     return 0;
 }
