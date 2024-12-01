aa4b7bf635a872a59ed904df6af7dbab766d28eb
Wayne McRae <waynem567@gmail.com>
2024-12-01 17:29:50 +0200

    Update helloworld.cpp

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -3,27 +3,15 @@
 /**
  * @brief Main entry point of the program
  *
- * This function initializes an array of prices and outputs each price to the console.
- * The array contains five elements, each representing a different price. The prices
- * are iterated and printed using standard output.
- *
- * @return 0 on successful execution
+ * The function calculates and prints the size of a double
+ * variable in bytes using the sizeof() operator.
  */
 int main() {
-    
-    double prices[5];
 
-    prices[0] = 10.99;
-    prices[1] = 12.50;
-    prices[2] = 9.99;
-    prices[3] = 7.50;
-    prices[4] = 6.00;
+    double pi = 3.14159;
+    int size = sizeof(pi);
 
-    std::cout << prices[0] << std::endl;
-    std::cout << prices[1] << std::endl;
-    std::cout << prices[2] << std::endl;
-    std::cout << prices[3] << std::endl;
-    std::cout << prices[4] << std::endl;
+    std::cout << "The size of pi is: " << size << " bytes" << std::endl;
 
     return 0;
 }
