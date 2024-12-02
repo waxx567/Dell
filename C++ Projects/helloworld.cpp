45c95c2c678f3a394ac7cc091b6ef5ba17a14501
Wayne McRae <waynem567@gmail.com>
2024-12-02 14:37:08 +0200

    Update helloworld.cpp

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 
+/**
+ * @brief The main entry point of the program
+ *
+ * @details This function demonstrates how to print the memory address of a
+ *          variable to the console.
+ *
+ * @return 0 on success
+ */
 int main() {
 
     // memory address = the location in memory where a variable is stored
@@ -9,7 +17,9 @@ int main() {
     int age = 30;
     bool isMale = true;
 
-    std::cout << "Name: " << &name << std::endl;
+    std::cout << "Address of name: " << &name << std::endl;
+    std::cout << "Address of age: " << &age << std::endl;
+    std::cout << "Address of isMale: " << &isMale << std::endl;
 
     return 0;
 }
