6fd8134710caef60a61f1dd262781b87b1faadf9
Wayne McRae <waynem567@gmail.com>
2024-12-02 06:57:24 +0200

    Update helloworld.cpp

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -1,52 +1,15 @@
 #include <iostream>
 
-int searchArray(std::string array[], int size, std::string element);
-
-/**
- * @brief The main entry point of the program
- *
- * @details This function asks the user to input their favorite food and checks if it is present in the array.
- *          If the food is present in the array, it outputs the food.
- *          Otherwise, it tells the user that the food was not found in the array.
- */
 int main() {
 
-    std::string foods[] = {"apple", "banana", "cherry", "date", "elderberry"};
-    int size = sizeof(foods) / sizeof(std::string);
-    int index;
-    std::string myFood;
+    // sort an array
+    // bubble sort
+    int arr[] = {5, 2, 8, 1, 9, 3, 6, 4, 7};
+    int size = sizeof(arr) / sizeof(arr[0]);
 
-    std::cout << "Enter your favorite food: ";
-    std::cin >> myFood;
-
-    index = searchArray(foods, size, myFood);
-
-    if (index != -1) {
-        std::cout << "Your favorite food is at index " << index << ": " << foods[index] << std::endl;
-    }
-    else {
-        std::cout << "Your favorite food " << myFood << " is not in the list" << std::endl;
+    for (int element : arr) {
+        std::cout << element << " ";
     }
 
     return 0;
-}
-
-/**
- * @brief Searches an array for an element and returns the index of the element if found
- *
- * @param array the array to search
- * @param size the size of the array
- * @param element the element to search for
- *
- * @return the index of the element if found, -1 otherwise
- */
-int searchArray(std::string array[], int size, std::string element) {
-
-    for (int i = 0; i < size; i++) {
-        if (array[i] == element) {
-            return i;
-        }
-    }
-
-    return -1;
 }
