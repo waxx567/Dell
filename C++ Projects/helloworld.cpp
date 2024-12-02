a464dfb9fe29421adffc4b315eca545d24a525bc
Wayne McRae <waynem567@gmail.com>
2024-12-02 13:26:41 +0200

    Update helloworld.cpp

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -53,7 +53,7 @@ int main() {
         }
     }
     std::cout << "***************************************************" << std::endl;
-    std::cout << "*                   RESULTS                       *" << std::endl;
+    std::cout << "*                    RESULTS                      *" << std::endl;
     std::cout << "***************************************************" << std::endl;
     std::cout << "You got " << score << " out of " << size << " questions correct!" << std::endl;
     std::cout << "Score: " << (score/size * 100) << std::endl;
