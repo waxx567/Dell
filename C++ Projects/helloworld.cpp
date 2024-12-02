08e05b9f4e54224ed061a0afe88ff0c1592c9403
Wayne McRae <waynem567@gmail.com>
2024-12-02 08:29:34 +0200

    Update helloworld.cpp

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -12,16 +12,19 @@ int main() {
 
     std::string bikes[5];
     int size = sizeof(bikes) / sizeof(bikes[0]);
+    std::string temp;
 
     for (int i = 0; i < size; i++) {
         std::cout << "Enter a bike or 'q' to quit #" << i + 1 << ": ";
-        std::getline(std::cin, bikes[i]);
+        std::getline(std::cin, temp);
 
-        if (bikes[i] == "q") {
+        if (temp == "q") {
             break;
         }
-
-        // the problem is that 'q' will be added to the array, which we don't want
+        else
+        {
+            bikes[i] = temp;
+        }
     }
 
     std::cout << "Your bikes are: " << std::endl;
