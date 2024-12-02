91212b2c7fafc1d86f753fba79f78145fb1a4ce6
Wayne McRae <waynem567@gmail.com>
2024-12-02 14:53:16 +0200

    Update helloworld.cpp

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -1,25 +1,20 @@
 #include <iostream>
 
-/**
- * @brief The main entry point of the program
- *
- * @details This function demonstrates how to print the memory address of a
- *          variable to the console.
- *
- * @return 0 on success
- */
 int main() {
 
-    // memory address = the location in memory where a variable is stored
-    //                  address can be accessed using the & symbol
+    // pass by value = pass the value of an object by reference
+    // pass by reference = pass the address of an object by reference
 
-    std::string name = "Wayne";
-    int age = 30;
-    bool isMale = true;
+    std::string x = "Cool";
+    std::string y = "Uncool";
+    std::string temp;
 
-    std::cout << "Address of name: " << &name << std::endl;
-    std::cout << "Address of age: " << &age << std::endl;
-    std::cout << "Address of isMale: " << &isMale << std::endl;
+    temp = x;
+    x = y;
+    y = temp;
+
+    std::cout << x << '\n';
+    std::cout << y << '\n';
 
     return 0;
 }
