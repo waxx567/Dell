e5f467e0f029f38ee2d049437889faefeaacb4e0
Wayne McRae <waynem567@gmail.com>
2024-12-02 08:00:17 +0200

    Update helloworld.cpp

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 
 /**
- * @brief Main entry point of the program
+ * @brief The main entry point of the program
  *
- * This function creates an array of SIZE elements and fills it with the string "Triumph".
- * It then prints out each element of the array to the console.
+ * @details This function initializes an array of strings representing bike brands,
+ *          fills the first half of the array with the string "Triumph" and the second
+ *          half with the string "Honda". It then iterates over the array and prints
+ *          each brand to the console.
  *
  * @return 0 on successful execution
  */
@@ -14,7 +16,8 @@ int main() {
 
     std::string bikes[SIZE];
 
-    fill(bikes, bikes + SIZE, "Triumph");
+    fill(bikes, bikes + (SIZE/2), "Triumph");
+    fill(bikes + (SIZE/2), bikes + SIZE, "Honda");
 
     for (std::string bike : bikes) {
         std::cout << bike << std::endl;
