d5d68484f4daaf42fe26ec2b979ba92a885f5228
Wayne McRae <waynem567@gmail.com>
2024-12-02 06:27:26 +0200

    Update helloworld.cpp

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -1,37 +1,52 @@
 #include <iostream>
 
-double getTotal(double prices[], int size);
+int searchArray(int array[], int size, int element);
 
 /**
  * @brief The main entry point of the program
  *
- * @details This function contains the main logic of the program.
- *          It creates an array of prices, calculates the total
- *          and prints it to the console.
+ * @details This function asks the user to input a number and checks if it is present in the array.
+ *          If the number is present in the array, it outputs the index of the number.
+ *          Otherwise, it tells the user that the number was not found in the array.
  */
 int main() {
 
-    // pass an array to a function
-    double prices[] = {10.99, 12.50, 9.99, 7.50, 6.00};
-    int size = sizeof(prices) / sizeof(prices[0]);
-    double total = getTotal(prices, size);
+    int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int size = sizeof(numbers) / sizeof(int);
+    int index;
+    int myNum;
 
-    std::cout << "Total: $" << total << std::endl;
+    std::cout << "Enter a number: ";
+    std::cin >> myNum;
+
+    index = searchArray(numbers, size, myNum);
+
+    if (index != -1) {
+        std::cout << "The number " << myNum << " is at index " << index << std::endl;
+    }
+    else {
+        std::cout << "The number " << myNum << " was not found in the array" << std::endl;
+    }
 
     return 0;
 }
 
 /**
- * @brief Calculates the total of an array of prices
+ * @brief Searches an array for an element and returns the index of the element if found
  *
- * @param prices the array of prices
+ * @param array the array to search
+ * @param size the size of the array
+ * @param element the element to search for
  *
- * @return the total of the prices
+ * @return the index of the element if found, -1 otherwise
  */
-double getTotal(double prices[], int size) {
-    double total = 0;
+int searchArray(int array[], int size, int element) {
+
     for (int i = 0; i < size; i++) {
-        total += prices[i];
+        if (array[i] == element) {
+            return i;
+        }
     }
-    return total;
+
+    return -1;
 }
