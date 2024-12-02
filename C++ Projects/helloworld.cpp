36df56c2886af7e3f9baad759ae09acae600706a
Wayne McRae <waynem567@gmail.com>
2024-12-02 11:22:19 +0200

    Update helloworld.cpp

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -1,13 +1,5 @@
 #include <iostream>
 
-/**
- * @brief Main entry point of the program
- *
- * This function creates a 2D array of strings and displays the contents of
- * the array to the user.
- *
- * @return 0 if the program is successful, non-zero otherwise.
- */
 int main() {
 
     std::string models[][3] = {
@@ -16,17 +8,12 @@ int main() {
         {"Camry", "Corolla", "RAV4"}
     };
 
-    std::cout << models[0][0] << " ";
-    std::cout << models[0][1] << " ";
-    std::cout << models[0][2] << std::endl;
+    int rows = sizeof(models) / sizeof(models[0]);
+    int columns = sizeof(models[0]) / sizeof(models[0][0]);
 
-    std::cout << models[1][0] << " ";
-    std::cout << models[1][1] << " ";
-    std::cout << models[1][2] << std::endl;
-
-    std::cout << models[2][0] << " ";
-    std::cout << models[2][1] << " ";
-    std::cout << models[2][2] << std::endl; 
+    for (int i = 0; i < rows; i++) {
+        std::cout << models[i] << std::endl;
+    }
 
     return 0;
 }
