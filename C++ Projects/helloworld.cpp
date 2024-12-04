9c77bd20cf648882d040834ab85d756b0d19a775
Wayne McRae <waynem567@gmail.com>
2024-12-04 08:56:04 +0200

    Update helloworld.cpp

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 
+/**
+ * @brief The main entry point of the program
+ *
+ * @details This function demonstrates the use of dynamic memory allocation in C++.
+ *          It allocates memory on the heap using the 'new' keyword, assigns a value
+ *          to the memory, prints the address and value of the memory, and then
+ *          deallocates the memory using the 'delete' keyword.
+ *
+ * @return 0 on successful execution
+ */
 int main() {
 
     // dynamic memory allocation = the process of creating memory on the heap
+    // whenever you use the 'new' operator, you should also use the 'delete' operator
     //                           = memory that is allocated after the program is compiled and running
     // Use the 'new' keyword to allocate memory on the heap
     // Use the 'delete' keyword to deallocate memory on the heap
