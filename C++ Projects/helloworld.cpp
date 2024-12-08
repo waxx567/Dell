55f1c2f5586d8ca3800f70bab088d39aa51df23c
Wayne McRae <waynem567@gmail.com>
2024-12-08 15:24:17 +0200

    Update helloworld.cpp

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -14,9 +14,25 @@ class Motorcycle {
         int year;
         std::string color;
         std::string licensePlate;
+
+        Motorcycle(std::string make, std::string model, int year, std::string color, std::string licensePlate) {
+            this->make = make;
+            this->model = model;
+            this->year = year;
+            this->color = color;
+            this->licensePlate = licensePlate;
+        }
 };
 
 int main() {
+
+    Motorcycle motorcycle1("Triumph", "Street Triple", 2023, "Yellow", "W4YN3M");
+
+    std::cout << "Make: " << motorcycle1.make << std::endl;
+    std::cout << "Model: " << motorcycle1.model << std::endl;
+    std::cout << "Year: " << motorcycle1.year << std::endl;
+    std::cout << "Color: " << motorcycle1.color << std::endl;
+    std::cout << "License Plate: " << motorcycle1.licensePlate << std::endl;
 
     return 0;
 }
