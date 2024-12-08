fd768b5d46d0ebaaf0ff958515330a6b01e1e8cf
Wayne McRae <waynem567@gmail.com>
2024-12-08 12:46:50 +0200

    objects

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -1,67 +1,13 @@
 #include <iostream>
 
-enum name {
-    Wayne,
-    John,
-    Jane,
-    Bob
-};
-
-// other examples
-
-enum Flavors {
-    VANILLA,
-    CHOCOLATE,
-    STRAWBERRY,
-    RASPBERRY
-};
-
-enum Colors {
-    RED,
-    GREEN,
-    BLUE
-};
-
-enum Days {
-    MONDAY = 1,
-    TUESDAY = 2,
-    WEDNESDAY = 3,
-    THURSDAY = 4,
-    FRIDAY = 5
-};
-
-enum parts {
-    gearlever = 56437,
-    steeringwheel = 97543,
-    seat = 12345,
-    engine = 23456,
-    transmission = 34567,
-    exhaust = 45678,
-    brake = 56789,
-    clutch = 67890
-};
+// objects = instances of classes
+//          can be used to store data
+//          can be used to perform actions
+//          can be used to represent real-world entities
+// to create an object, you must create a class
+// classes act as blueprints for creating objects
 
 int main() {
-
-    name firstName = Jane;
-
-    switch (firstName) {
-    case Wayne:
-        std::cout << "Wayne" << std::endl;
-        break;
-    case John:
-        std::cout << "John" << std::endl;
-        break;
-    case Jane:
-        std::cout << "Jane" << std::endl;
-        break;
-    case Bob:
-        std::cout << "Bob" << std::endl;
-        break;
-    default:
-        std::cout << "Unknown" << std::endl;
-        break;
-    }
 
     return 0;
 }
