5cbcf13f3f6d9c100137c6704a08686e623d8aa9
Wayne McRae <waynem567@gmail.com>
2024-12-08 15:16:42 +0200

    constructor

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -1,50 +1,22 @@
 #include <iostream>
 
-class Car {
+// constructor = functions that are used to create an object
+//               they are used to initialize the object
+//               they are used to set the initial values of the object
+//               they are used to perform actions on the object
+//               special methods that are called when an object is instantiated
+//               useful for assigning values to attributes as arguments
+
+class Motorcycle {
     public:
         std::string make;
         std::string model;
         int year;
         std::string color;
-
-        void accelerate() {
-            std::cout << "The " << make << " is accelerating" << std::endl;
-        }
-        void brake() {
-            std::cout << "The " << make << " is braking" << std::endl;
-        }
+        std::string licensePlate;
 };
 
 int main() {
-
-    Car car1;
-    Car car2;
-
-    car1.make = "Lexus";
-    car1.model = "IS300";
-    car1.year = 2022;
-    car1.color = "White";
-
-    car2.make = "Honda";
-    car2.model = "Civic";
-    car2.year = 2021;
-    car2.color = "Red";
-
-    std::cout << "Make: " << car1.make << std::endl;
-    std::cout << "Model: " << car1.model << std::endl;
-    std::cout << "Year: " << car1.year << std::endl;
-    std::cout << "Color: " << car1.color << std::endl;
-
-    std::cout << "Make: " << car2.make << std::endl;
-    std::cout << "Model: " << car2.model << std::endl;
-    std::cout << "Year: " << car2.year << std::endl;
-    std::cout << "Color: " << car2.color << std::endl;
-
-    car1.accelerate();
-    car1.brake();
-
-    car2.accelerate();
-    car2.brake();
 
     return 0;
 }
