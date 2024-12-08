ade67ca94bb751944717741f3deca883fbcdd211
Wayne McRae <waynem567@gmail.com>
2024-12-08 18:00:48 +0200

    Update helloworld.cpp

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -3,7 +3,20 @@
 // overloaded constructors = multiple constructors with the same name but different parameters
 // allows fo varying arguments when instantiating an object
 
+class Pizza {
+    public:
+        std::string topping1;
+
+    Pizza(std::string topping1) {
+        this->topping1 = topping1;
+    }
+};
+
 int main() {
+
+    Pizza pizza1("Pepperoni");
+
+    std::cout << pizza1.topping1 << std::endl;
 
     return 0;
 }
