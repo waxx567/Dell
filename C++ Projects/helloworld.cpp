b1d6776f06b2ddbf8ef115d4917aa9d33e42b848
Wayne McRae <waynem567@gmail.com>
2024-12-09 07:01:13 +0200

    getters and setters

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -1,39 +1,11 @@
 #include <iostream>
 
-// overloaded constructors = multiple constructors with the same name but different parameters
-// allows fo varying arguments when instantiating an object
-
-class Pizza {
-    public:
-        std::string topping1;
-        std::string topping2;
-
-    // so adding this
-    Pizza() {
-
-    }
-    // solves the problem
-    // but we can see how inefficient this is
-    Pizza(std::string topping1) {
-        this->topping1 = topping1;
-    }
-
-    Pizza(std::string topping1, std::string topping2) {
-        this->topping1 = topping1;
-        this->topping2 = topping2;
-    }
-};
+// abstraction = hiding the implementation details of a class or function
+//               from the user
+// getter = a function that returns the value of a private member variable
+// setter = a function that sets the value of a private member variable
 
 int main() {
-
-    Pizza pizza1("Pepperoni");
-    Pizza pizza2("Mushroom", "Peppers");
-    // what if we want a plain pizza with no toppings
-    Pizza pizza3;
-    // but we still throw an error because Pizza cannot be called with no parameters
-
-    std::cout << pizza2.topping1 << std::endl;
-    std::cout << pizza2.topping2 << std::endl;
 
     return 0;
 }
