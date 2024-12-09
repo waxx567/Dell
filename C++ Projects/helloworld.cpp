901e0c3915f671b77846fcfe6f5c49627e94fcbd
Wayne McRae <waynem567@gmail.com>
2024-12-09 07:36:19 +0200

    Update helloworld.cpp

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -5,7 +5,16 @@
 // getter = a function that returns the value of a private member variable
 // setter = a function that sets the value of a private member variable
 
+class Stove {
+    public:
+        int temperature = 0;
+    
+};
+
 int main() {
+
+    Stove stove;
+    // since temperature is public, it is accessible outside the class and can be modified
 
     return 0;
 }
