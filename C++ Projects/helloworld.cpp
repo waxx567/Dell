d4b41bc73a224594f0e1167ea0cc4238992c8310
Wayne McRae <waynem567@gmail.com>
2024-12-09 09:31:57 +0200

    Update helloworld.cpp

--- C++ Projects/helloworld.cpp
+++ C++ Projects/helloworld.cpp
@@ -11,6 +11,11 @@ class Shape {
 class Cube : public Shape {
     public:
     double side;   
+    /**
+     * @brief Constructor to create a cube with given side length
+     *
+     * @param side The length of each side of the cube
+     */
     Cube(double side) {
         this->side = side;
         this->area = 6 * side * side;
@@ -20,6 +25,13 @@ class Cube : public Shape {
 class Sphere : public Shape {
     public:
     double radius;
+    /**
+     * @brief Constructor to create a sphere with given radius
+     *
+     * @param radius The radius of the sphere
+     *
+     * @details Calculates the area and volume of the sphere based on the radius
+     */
     Sphere(double radius) {
         this->radius = radius;
         this->area = 4 * 3.14159 * (radius * radius);
@@ -27,6 +39,14 @@ class Sphere : public Shape {
     }
 };
 
+/**
+ * @brief The main entry point of the program
+ *
+ * This function creates a Cube and a Sphere object with a side length and radius of 10, respectively.
+ * It calculates their areas and volumes, and outputs the results to the console.
+ *
+ * @return 0 on successful execution
+ */
 int main() {
 
     Cube cube(10);
