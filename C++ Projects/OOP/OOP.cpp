43023079ee7408c2b85b7fd791f8062240bd567a
Wayne McRae <waynem567@gmail.com>
2024-12-09 10:12:16 +0200

    Update OOP.cpp

--- C++ Projects/OOP/OOP.cpp
+++ C++ Projects/OOP/OOP.cpp
@@ -2,12 +2,18 @@
 using namespace std;
 
 class Employee {
+public:
     string Name;
     string Company;
     int Age;
 };
 int main() {
-
-    int number;
     Employee employee1;
+    employee1.Name = "John Doe";
+    employee1.Company = "ABC Company";
+    employee1.Age = 30;
+    cout << employee1.Name << endl;
+    cout << employee1.Company << endl;
+    cout << employee1.Age << endl;
+    return 0;
 }
