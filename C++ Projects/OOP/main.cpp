e6ba24bd379b585875b7cbdbaf3323c05b9483a9
Wayne McRae <waynem567@gmail.com>
2024-12-10 08:00:25 +0200

    Update main.cpp

--- C++ Projects/OOP/main.cpp
+++ C++ Projects/OOP/main.cpp
@@ -2,12 +2,18 @@
 #include <tuple>
 using namespace std;
 
-// changing elements in tuples 
+// declare tuple and then add elements using make_tuple
 
 int main() {
     tuple <int, string> person(1, "John Doe");
     cout << get<1>(person) << endl;
     get<1>(person) = "Jane Doe";
     cout << get<1>(person) << endl;
+
+    tuple <int, char, bool, float> values;
+    values = make_tuple(1, 'a', true, 3.14);
+    cout << get<1>(values) << endl;
+    cout << get<3>(values) << endl;
+
     return 0;
 }
