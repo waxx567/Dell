993b0f053fee07aa1a0a45deb80103aa705094de
Wayne McRae <waynem567@gmail.com>
2024-12-10 08:08:56 +0200

    Update main.cpp

--- C++ Projects/OOP/main.cpp
+++ C++ Projects/OOP/main.cpp
@@ -12,7 +12,9 @@ int main() {
 
     tuple <int, char, bool, float> values;
     // values = make_tuple(1, 'a', true, 3.14);
+    cout << get<0>(values) << endl;
     cout << get<1>(values) << endl;
+    cout << get<2>(values) << endl;
     cout << get<3>(values) << endl;
 
     return 0;
