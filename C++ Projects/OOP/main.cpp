89916aa7bfc56c8efa3c4a0c99fa15d94a0653f7
Wayne McRae <waynem567@gmail.com>
2024-12-10 08:16:50 +0200

    swapping the contents of two tuples

--- C++ Projects/OOP/main.cpp
+++ C++ Projects/OOP/main.cpp
@@ -2,19 +2,25 @@
 #include <tuple>
 using namespace std;
 
-// another way to initialize
+// swapping the contents of two tuples
 
 int main() {
-    tuple <int, string> person(1, "John Doe");
-    cout << get<1>(person) << endl;
-    get<1>(person) = "Jane Doe";
-    cout << get<1>(person) << endl;
+    // tuple <int, string> person(1, "John Doe");
+    // cout << get<1>(person) << endl;
+    // get<1>(person) = "Jane Doe";
+    // cout << get<1>(person) << endl;
 
-    tuple <int, char, bool, float> values = make_tuple(1, 'a', true, 3.14);
-    cout << get<0>(values) << endl;
-    cout << get<1>(values) << endl;
-    cout << get<2>(values) << endl;
-    cout << get<3>(values) << endl;
+    // tuple <int, char, bool, float> values;
+    // values = make_tuple(1, 'a', true, 3.14);
+    // cout << get<1>(values) << endl;
+    // cout << get<3>(values) << endl;
+
+    tuple <int, int> numbers(1, 2);
+    cout << get<0>(numbers) << endl;
+    cout << get<1>(numbers) << endl;
+    swap(get<0>(numbers), get<1>(numbers));
+    cout << get<0>(numbers) << endl;
+    cout << get<1>(numbers) << endl;
 
     return 0;
 }
