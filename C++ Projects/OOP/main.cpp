d53cd57209e8674218cef3370b4a71832b01247c
Wayne McRae <waynem567@gmail.com>
2024-12-10 07:51:11 +0200

    Update main.cpp

--- C++ Projects/OOP/main.cpp
+++ C++ Projects/OOP/main.cpp
@@ -2,11 +2,12 @@
 #include <tuple>
 using namespace std;
 
-// tuples 
+// changing elements in tuples 
 
 int main() {
     tuple <int, string> person(1, "John Doe");
-    cout << get<0>(person) << endl;
+    cout << get<1>(person) << endl;
+    get<1>(person) = "Jane Doe";
     cout << get<1>(person) << endl;
     return 0;
 }
