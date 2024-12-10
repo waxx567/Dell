f71e6528c423817102be81600228563614ec86ab
Wayne McRae <waynem567@gmail.com>
2024-12-10 08:11:21 +0200

    another way to initialize

--- C++ Projects/OOP/main.cpp
+++ C++ Projects/OOP/main.cpp
@@ -2,7 +2,7 @@
 #include <tuple>
 using namespace std;
 
-// what you get if you declare but don't initialize
+// another way to initialize
 
 int main() {
     tuple <int, string> person(1, "John Doe");
@@ -10,8 +10,7 @@ int main() {
     get<1>(person) = "Jane Doe";
     cout << get<1>(person) << endl;
 
-    tuple <int, char, bool, float> values;
-    // values = make_tuple(1, 'a', true, 3.14);
+    tuple <int, char, bool, float> values = make_tuple(1, 'a', true, 3.14);
     cout << get<0>(values) << endl;
     cout << get<1>(values) << endl;
     cout << get<2>(values) << endl;
