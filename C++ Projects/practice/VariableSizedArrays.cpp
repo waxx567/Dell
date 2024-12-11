6bff2a4fbe06b13d42c1208cbfa04b9156a5517d
Wayne McRae <waynem567@gmail.com>
2024-12-11 09:00:59 +0200

    Update VariableSizedArrays.cpp

--- C++ Projects/practice/VariableSizedArrays.cpp
+++ C++ Projects/practice/VariableSizedArrays.cpp
@@ -5,6 +5,13 @@
 #include <algorithm>
 using namespace std;
 
+/*
+https://www.hackerrank.com/challenges/variable-sized-arrays/problem?isFullScreen=true
+
+Consider an n-element array, a, where each index i in the array contains a reference to an array of k^i integers (where the value of k^i varies from array to array). See the Explanation section below for a diagram.
+
+Given a, you must answer q queries. Each query is in the format i j, where i denotes an index in array a and j denotes an index in the array located at a[i]. For each query, find and print the value of element j in the array at location a[i] on a new line.
+*/
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
