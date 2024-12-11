11be3c7db0fac4e781d4dbbd9131124e7aba4f09
Wayne McRae <waynem567@gmail.com>
2024-12-11 12:56:31 +0200

    Update sets-stl.cpp

--- C++ Projects/practice/sets-stl.cpp
+++ C++ Projects/practice/sets-stl.cpp
@@ -5,6 +5,38 @@
 #include <algorithm>
 using namespace std;
 
+/*
+https://www.hackerrank.com/challenges/vector-sort/problem?isFullScreen=true
+
+Sets are a part of the C++ STL. Sets are containers that store unique elements following a specific order. 
+
+Input Format
+
+The first line of the input contains Q where Q is the number of queries. The next Q lines contain 1 query each. Each query consists of two integers y and z where z is the type of the query and  is an integer.
+
+Output Format
+
+For queries of type 3 print "Yes"(without quotes) if the number z is present in the set and if the number is not present, then print "No"(without quotes).
+Each query of type 3 should be printed in a new line.
+
+Sample Input
+
+8
+1 9
+1 6
+1 10
+1 4
+3 6
+3 14
+2 6
+3 6
+
+Sample Output
+
+Yes
+No
+No
+*/
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
