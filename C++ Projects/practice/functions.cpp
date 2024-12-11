60590f9f198cafc6458f8a1d9e2c4f0b51aa3587
Wayne McRae <waynem567@gmail.com>
2024-12-11 07:31:49 +0200

    Update functions.cpp

--- C++ Projects/practice/functions.cpp
+++ C++ Projects/practice/functions.cpp
@@ -37,8 +37,20 @@ int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
     int a, b, c, d;
     cin >> a >> b >> c >> d;
-    
+
     cout << greatestInt(a, b, c, d);    
 
     return 0;
+}
+
+int greatestInt(int a, int b, int c, int d) {
+    if (a >= b && a >= c && a >= d) {
+        return a;
+    } else if (b >= a && b >= c && b >= d) {
+        return b;
+    } else if (c >= a && c >= b && c >= d) {
+        return c;
+    } else {
+        return d;
+    }
 }
