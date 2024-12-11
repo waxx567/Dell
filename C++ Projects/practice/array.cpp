705e6b2b9e41b9877f89929bb55e6c0e5034b730
Wayne McRae <waynem567@gmail.com>
2024-12-11 08:24:16 +0200

    Update array.cpp

--- C++ Projects/practice/array.cpp
+++ C++ Projects/practice/array.cpp
@@ -5,6 +5,36 @@
 #include <algorithm>
 using namespace std;
 
+/*
+https://www.hackerrank.com/challenges/c-tutorial-array/problem?isFullScreen=true
+
+An array is a series of elements of the same type placed in contiguous memory locations that can be individually referenced by adding an index to a unique identifier.
+
+You will be given an array of N integers and you have to print the integers in the reverse order.
+
+Input Format
+
+The first line of the input contains ,where N is the number of integers.The next line contains N space-separated integers.
+
+Constraints
+
+1 <= N <= 1000
+1 <= A[i] <= 1000, where A[i] is the ith integer in the array.
+
+Output Format
+
+Print the N integers of the array in the reverse order, space-separated on a single line.
+
+Sample Input
+
+4
+1 4 3 2
+
+Sample Output
+
+2 3 4 1
+*/
+
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
