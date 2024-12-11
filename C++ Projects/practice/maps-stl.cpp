c5f5649e9822f90b3004c1ffc019f657c331573a
Wayne McRae <waynem567@gmail.com>
2024-12-11 13:46:42 +0200

    Update maps-stl.cpp

--- C++ Projects/practice/maps-stl.cpp
+++ C++ Projects/practice/maps-stl.cpp
@@ -5,6 +5,11 @@
 #include <algorithm>
 using namespace std;
 
+/*
+https://www.hackerrank.com/challenges/vector-sort/problem?isFullScreen=true
+
+Maps are a part of the C++ STL.Maps are associative containers that store elements formed by a combination of a key value and a mapped value, following a specific order.
+*/
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
