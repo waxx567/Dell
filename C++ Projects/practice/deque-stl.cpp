a39c0a60dd395e3857b001b4dcd7930384aed572
Wayne McRae <waynem567@gmail.com>
2024-12-11 15:15:22 +0200

    Update deque-stl.cpp

---
+++ C++ Projects/practice/deque-stl.cpp
@@ -0,0 +1,38 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+/*
+https://www.hackerrank.com/challenges/vector-sort/problem?isFullScreen=true
+
+Double ended queue or Deque(part of C++ STL) are sequence containers with dynamic sizes that can be expanded or contracted on both ends (either its front or its back).
+
+Input Format
+
+First line of input will contain the number of test cases T. For each test case, you will be given the size of array N and the size of subarray to be used K. This will be followed by the elements of the array Ai.
+
+Output Format
+
+For each of the contiguous subarrays of size K of each array, you have to print the maximum integer.
+
+Sample Input
+
+2
+5 2
+3 4 6 3 4
+7 4
+3 4 5 8 1 4 10
+
+Sample Output
+
+4 6 6 4
+8 8 8 10
+*/
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    return 0;
+}
