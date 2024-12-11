d952e4da9675d992052d633fd921931627c00d99
Wayne McRae <waynem567@gmail.com>
2024-12-11 17:33:43 +0200

    Update exception-handling.cpp

--- C++ Projects/practice/exception-handling.cpp
+++ C++ Projects/practice/exception-handling.cpp
@@ -38,7 +38,42 @@ result=3
 returning control flow to caller
 */
 
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stdexcept>
+using namespace std;
+
+int largest_proper_divisor(int n) {
+    if (n == 0) {
+        throw invalid_argument("largest proper divisor is not defined for n=0");
+    }
+    if (n == 1) {
+        throw invalid_argument("largest proper divisor is not defined for n=1");
+    }
+    for (int i = n / 2; i >= 1; --i) {
+        if (n % i == 0) {
+            return i;
+        }
+    }
+    return -1; 
+}
+
+void process_input(int n) {
+    try {
+        int result = largest_proper_divisor(n);
+        cout << "result=" << result << endl;
+    } catch (invalid_argument &e) {
+        cout << e.what() << endl;
+    }
+    cout << "returning control flow to caller" << endl;
+}
+
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n;
+    cin >> n;
+    process_input(n);
     return 0;
 }
