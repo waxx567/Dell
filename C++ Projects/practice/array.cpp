4be69908e3419cf2382845170f49a570abb9778d
Wayne McRae <waynem567@gmail.com>
2024-12-11 08:34:43 +0200

    Update array.cpp

--- C++ Projects/practice/array.cpp
+++ C++ Projects/practice/array.cpp
@@ -37,6 +37,20 @@ Sample Output
 
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    int N;
+    cin >> N; 
+    
+    vector<int> arr(N);
+    
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+    
+    for (int i = N-1; i >= 0; i--) {
+        cout << i;
+    }
+    cout << endl;
+     
     return 0;
 }
