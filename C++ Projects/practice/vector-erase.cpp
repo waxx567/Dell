43b42fc0313070d9f14ad62c7eaa2d8949baf988
Wayne McRae <waynem567@gmail.com>
2024-12-11 11:23:44 +0200

    Update vector-erase.cpp

--- C++ Projects/practice/vector-erase.cpp
+++ C++ Projects/practice/vector-erase.cpp
@@ -8,7 +8,7 @@ using namespace std;
 /*
 https://www.hackerrank.com/challenges/vector-erase/problem?isFullScreen=true
 
-You are provided with a vector of  integers. Then, you are given  queries. For the first query, you are provided with  integer, which denotes a position in the vector. The value at this position in the vector needs to be erased. The next query consists of  integers denoting a range of the positions in the vector. The elements which fall under that range should be removed. The second query is performed on the updated vector which we get after performing the first query.
+You are provided with a vector of N integers. Then, you are given  queries. For the first query, you are provided with  integer, which denotes a position in the vector. The value at this position in the vector needs to be erased. The next query consists of  integers denoting a range of the positions in the vector. The elements which fall under that range should be removed. The second query is performed on the updated vector which we get after performing the first query.
 
 Input Format
 
