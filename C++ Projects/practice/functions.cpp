67257a7bfb374721633210682c013bd3c3191d37
Wayne McRae <waynem567@gmail.com>
2024-12-11 07:10:11 +0200

    Update functions.cpp

---
+++ C++ Projects/practice/functions.cpp
@@ -0,0 +1,44 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+/*
+https://www.hackerrank.com/challenges/c-tutorial-functions/problem?isFullScreen=true
+
+Functions are a bunch of statements glued together. A function is provided with zero or more arguments, and it executes the statements on it. Based on the return type, it either returns nothing (void) or something.
+
+Input Format
+
+Input will contain four integers -  , one per line.
+
+Output Format
+
+Return the greatest of the four integers.
+PS: I/O will be automatically handled.
+
+Sample Input
+
+3
+4
+6
+5
+
+Sample Output
+
+6
+*/
+
+int greatestInt(int a, int b, int c, int d);
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    
+    cout << greatestInt(a, b, c, d);    
+
+    return 0;
+}
