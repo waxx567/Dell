541e1fb6d3a0a346f3057715ac5bc78d61021d5f
Wayne McRae <waynem567@gmail.com>
2024-12-11 16:02:38 +0200

    Update exception-handling.cpp

--- C++ Projects/practice/exception-handling.cpp
+++ C++ Projects/practice/exception-handling.cpp
@@ -5,6 +5,38 @@
 #include <algorithm>
 using namespace std;
 
+/*
+https://www.hackerrank.com/challenges/vector-sort/problem?isFullScreen=true
+
+In this challenge, the task is to debug the existing code to successfully execute all provided test files.
+
+You are required to extend the existing code so that it handles std::invalid_argument exception properly. 
+
+Input Format
+
+The input is read by the provided locked code template. In the only line of the input, there is a single integer n, which is going to be the argument passed to function process_input.
+
+Output Format
+
+The output should be produced by function process_input as described in the statement.
+
+Sample Input 0
+
+0
+
+Sample Output 0
+
+largest proper divisor is not defined for n=0
+returning control flow to caller
+Sample Input 1
+
+9
+
+Sample Output 1
+
+result=3
+returning control flow to caller
+*/
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
