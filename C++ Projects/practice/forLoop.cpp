23be682bc6126f92b5333411cc34c3e5c75783fe
Wayne McRae <waynem567@gmail.com>
2024-12-11 06:44:45 +0200

    Update forLoop.cpp

--- C++ Projects/practice/forLoop.cpp
+++ C++ Projects/practice/forLoop.cpp
@@ -52,9 +52,45 @@ odd
 
 using namespace std;
 
+void NumbersToWords(int a, int b);
+
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
 
+    int a, b;
+    cin >> a >> b;
+
+    NumbersToWords(a, b);
 
     return 0;
+}
+
+void NumbersToWords(int a, int b) {
+    for (int i = a; i <= b; i++) {
+        if (i >= 1 && i <= 9) {
+            if (i == 1) {
+                cout << "one" << endl;
+            } else if (i == 2) {
+                cout << "two" << endl;
+            } else if (i == 3) {
+                cout << "three" << endl;
+            } else if (i == 4) {
+                cout << "four" << endl;
+            } else if (i == 5) {
+                cout << "five" << endl;
+            } else if (i == 6) {
+                cout << "six" << endl;
+            } else if (i == 7) {
+                cout << "seven" << endl;
+            } else if (i == 8) {
+                cout << "eight" << endl;
+            } else if (i == 9) {
+                cout << "nine" << endl;
+            }
+        } else if (i > 9 && i % 2 == 0) {
+            cout << "even" << endl;
+        } else if (i > 9 && i % 2 != 0) {
+            cout << "odd" << endl;
+        }
+    }
 }
