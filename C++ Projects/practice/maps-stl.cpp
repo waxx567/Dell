6ce0d3e3cdd9cdfdd7d390dffba440647791503b
Wayne McRae <waynem567@gmail.com>
2024-12-11 13:49:35 +0200

    Update maps-stl.cpp

--- C++ Projects/practice/maps-stl.cpp
+++ C++ Projects/practice/maps-stl.cpp
@@ -9,6 +9,31 @@ using namespace std;
 https://www.hackerrank.com/challenges/vector-sort/problem?isFullScreen=true
 
 Maps are a part of the C++ STL.Maps are associative containers that store elements formed by a combination of a key value and a mapped value, following a specific order.
+
+Input Format
+
+The first line of the input contains Q where Q is the number of queries. The next Q lines contain 1 query each.The first integer, `type` of each query is the type of the query. If query is of type 1, it consists of one string and an integer X and Y where X is the name of the student and Y is the marks of the student. If query is of type 2 or 3,it consists of a single string X where X is the name of the student.
+
+Output Format
+
+For queries of type 3 print the marks of the given student.
+
+Sample Input
+
+7
+1 Jesse 20
+1 Jess 12
+1 Jess 18
+3 Jess
+3 Jesse
+2 Jess
+3 Jess
+
+Sample Output
+
+30
+20
+0
 */
 
 int main() {
