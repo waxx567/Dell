d0254be55e12758eeaa715cb1aa8060c074c5d61
Wayne McRae <waynem567@gmail.com>
2024-12-11 06:38:35 +0200

    Update forLoop.cpp

--- C++ Projects/practice/forLoop.cpp
+++ C++ Projects/practice/forLoop.cpp
@@ -23,10 +23,38 @@
 
 /*
 https://www.hackerrank.com/challenges/c-tutorial-for-loop/problem?isFullScreen=true
+
+Input Format
+
+You will be given two positive integers, a and b (a <= b), separated by a newline.
+
+Output Format
+
+For each integer n in the inclusive interval [a, b]:
+
+    - If 1 <= n <= 9, then print the English representation of it in lowercase. That is "one" for 1, "two" for 2, and so on.
+    - Else if n > 9 and it is an even number, then print "even".
+    - Else if n > 9 and it is an odd number, then print "odd".
+
+Sample Input
+
+8
+11
+
+Sample Output
+
+eight
+nine
+even
+odd
+
 */
 
 using namespace std;
+
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+
+
     return 0;
 }
