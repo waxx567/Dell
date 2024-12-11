38161231706d5e33d77bace176e0367320661675
Wayne McRae <waynem567@gmail.com>
2024-12-11 11:05:31 +0200

    Update vector-sort.cpp

--- C++ Projects/practice/vector-sort.cpp
+++ C++ Projects/practice/vector-sort.cpp
@@ -5,6 +5,29 @@
 #include <algorithm>
 using namespace std;
 
+/*
+https://www.hackerrank.com/challenges/vector-sort/problem?isFullScreen=true
+
+You are given N integers.Sort the N integers and print the sorted order.
+Store the N integers in a vector.Vectors are sequence containers representing arrays that can change in size.
+
+Input Format
+
+The first line of the input contains N where N is the number of integers. The next line contains N integers.
+
+Output Format
+
+Print the integers in the sorted order one by one in a single line followed by a space.
+
+Sample Input
+
+5
+1 6 10 8 4
+
+Sample Output
+
+1 4 6 8 10
+*/
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
