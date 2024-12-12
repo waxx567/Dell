f3badc3b99749aead18d8f293f39ac3e6e0823ac
Wayne McRae <waynem567@gmail.com>
2024-12-12 06:36:11 +0200

    Update messages-order.cpp

--- C++ Projects/practice/messages-order.cpp
+++ C++ Projects/practice/messages-order.cpp
@@ -5,6 +5,13 @@
 #include <algorithm>
 using namespace std;
 
+/*
+https://www.hackerrank.com/challenges/messages-order/problem?isFullScreen=true
+
+In real life applications and systems, a common component is a messaging system. Thea idea is that a sender sends messages to the recipient. The messages might be sent for example over the network. However, some network protocols don't guarantee to preserve the order of sent messages while they are received by the recipient. For example, if someone sends a text messages hello, hi and what's up, they might be received in the order what's up, hello, hi. In many systems the expected behavior is to preserve the order, so the order of sent messages is the same as the order of received messages.
+
+In this problem, the task is to implement a software layer over the top of a network protocol sending messages in arbitrary order, in such a way that the sent messages are printed by the recipient in the order they were sent.
+*/
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
