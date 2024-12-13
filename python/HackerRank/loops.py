9b3ec397d11f32cd776a18fa44e11d3c977a7ddd
Wayne McRae <waynem567@gmail.com>
2024-12-13 15:28:00 +0200

    Create loops.py

---
+++ python/HackerRank/loops.py
@@ -0,0 +1,8 @@
+# Task
+# The provided code stub reads and integer, , from STDIN. For all non-negative integers , print i^2
+
+if __name__ == '__main__':
+    n = int(input())
+    
+    for i in range(n):
+        print(i**2)
