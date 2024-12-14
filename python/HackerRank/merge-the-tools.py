e5b1b9dcf60d5e7312bf33b3bac37f501e4d417a
Wayne McRae <waynem567@gmail.com>
2024-12-14 12:55:38 +0200

    Update merge-the-tools.py

--- python/HackerRank/merge-the-tools.py
+++ python/HackerRank/merge-the-tools.py
@@ -2,18 +2,20 @@
 
 # Consider the following:
 
-# A string, , of length  where .
-# An integer, , where  is a factor of .
-# We can split  into  substrings where each subtring, , consists of a contiguous block of  characters in . Then, use each  to create string  such that:
+# A string, s, of length n where c^0c^1...c^n-1.
+# An integer, k, where k is a factor of n.
+# We can split s into n/k substrings where each subtring, t^i, consists of a contiguous block of k characters in s. Then, use each t to create string u^i such that:
 
-# The characters in  are a subsequence of the characters in .
-# Any repeat occurrence of a character is removed from the string such that each character in  occurs exactly once. In other words, if the character at some index  in  occurs at a previous index  in , then do not include the character in string .
-# Given  and , print  lines where each line  denotes string .
+# The characters in u^i are a subsequence of the characters in t^i.
+# Any repeat occurrence of a character is removed from the string such that each character in u^i occurs exactly once. In other words, if the character at some index j in t occurs at a previous index <j in t, then do not include the character in string u^i.
+# Given s and k, print n/k lines where each line i denotes string u^i.
 
 # Example
 
+# s = "AAABCADDE"
+# k = 3
 
-# There are three substrings of length  to consider: 'AAA', 'BCA' and 'DDE'. The first substring is all 'A' characters, so . The second substring has all distinct characters, so . The third substring has  different characters, so . Note that a subsequence maintains the original order of characters encountered. The order of characters in each subsequence shown is important.
+# There are three substrings of length 3 to consider: 'AAA', 'BCA' and 'DDE'. The first substring is all 'A' characters, so u^1 = 'A'. The second substring has all distinct characters, so u^2 = 'BCA'. The third substring has 2 different characters, so u^3 = 'DE'. Note that a subsequence maintains the original order of characters encountered. The order of characters in each subsequence shown is important.
 
 # Function Description
 
@@ -23,35 +25,44 @@
 
 # string s: the string to analyze
 # int k: the size of substrings to analyze
+
 # Prints
 
-# Print each subsequence on a new line. There will be  of them. No return value is expected.
+# Print each subsequence on a new line. There will be n/k of them. No return value is expected.
 
 # Input Format
 
-# The first line contains a single string, .
-# The second line contains an integer, , the length of each substring.
+# The first line contains a single string, s.
+# The second line contains an integer, k, the length of each substring.
 
 # Constraints
 
-# , where  is the length of 
-# It is guaranteed that  is a multiple of .
+# 1 <= n <= 10^4, where n is the length of s
+# 1 <= k <= n
+# It is guaranteed that n is a multiple of k.
+
 # Sample Input
 
 # STDIN       Function
 # -----       --------
 # AABCAAADA   s = 'AABCAAADA'
 # 3           k = 3
+
 # Sample Output
 
 # AB
 # CA
 # AD
+
 # Explanation
 
-# Split  into  equal parts of length . Convert each  to  by removing any subsequent occurrences of non-distinct characters in :
+# Split s into n/k = 9/3 = 3 equal parts of length . Convert each t^i to u^i by removing any subsequent occurrences of non-distinct characters in t^i:
 
-# Print each  on a new line.
+# t^0 = AAB -> u^0 = AB
+# t^1 = CAA -> u^1 = CA
+# t^2 = ADA -> u^2 = AD
+
+# Print each u^i on a new line.
 
 def merge_the_tools(string, k):
     # your code goes here
