df8434f3eea44fe6320764df86223e1d71a644fa
Wayne McRae <waynem567@gmail.com>
2024-12-14 07:21:28 +0200

    Create find-angle-mbc.py

---
+++ python/HackerRank/find-angle-mbc.py
@@ -0,0 +1,24 @@
+# https://www.hackerrank.com/challenges/find-angle/problem?isFullScreen=true
+
+# Task
+
+# ABC is a right-angled triangle at B.
+# Therefore, the angle ABC is a right angle.
+# Point M is the midoint of hypothenuse AC.
+# You are given the lengths AB and BC.
+# Your task is to find the measure of angle MBC in degrees.
+
+# Input Format
+
+# The first line contains the length of side AB.
+# The second line contains the length of side BC.
+
+# Constraints
+
+# 0 < AB <= 100
+# 0 < BC <= 100
+# Lengths AB and BC are natural numbers.
+
+# Output Format
+
+# Print the angle MBC in degrees.
