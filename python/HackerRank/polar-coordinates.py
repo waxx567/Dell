907048eb432d72f8454a22fab33ded6f336e97ae
Wayne McRae <waynem567@gmail.com>
2024-12-14 07:09:44 +0200

    Update polar-coordinates.py

--- python/HackerRank/polar-coordinates.py
+++ python/HackerRank/polar-coordinates.py
@@ -1,4 +1,28 @@
+# 
 # https://www.hackerrank.com/challenges/polar-coordinates/problem?isFullScreen=true
 
 # Task
 # You are given a complex . Your task is to convert it to polar coordinates.
+
+# Constraints
+
+# Given number is a valid complex number
+
+# Sample Input
+
+#   1+2j
+
+# Sample Output
+
+#  2.23606797749979 
+#  1.1071487177940904
+
+# Note: The output should be correct up to 3 decimal places.
+# 
+
+import cmath
+
+r = complex(input())
+
+print(abs(r))
+print(cmath.phase(r))
