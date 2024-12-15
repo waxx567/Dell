89f9f13919dfa9bb034bda73040d5bf7b38d8186
Wayne McRae <waynem567@gmail.com>
2024-12-15 07:12:12 +0200

    Update sum-and-prod.py

--- python/HackerRank/sum-and-prod.py
+++ python/HackerRank/sum-and-prod.py
@@ -60,3 +60,9 @@
 # The product of this sum = 24
 
 import numpy
+
+n, m = map(int, input().split())
+
+a = numpy.array([input().split() for _ in range(n)], int)
+
+print(numpy.prod(numpy.sum(a, axis = 0)))
