f649d807054221fa34341c10fba2634fc4b4d497
Wayne McRae <waynem567@gmail.com>
2024-12-15 09:21:14 +0200

    Update operators.py

--- python/HackerRank/30 Days of Code/operators.py
+++ python/HackerRank/30 Days of Code/operators.py
@@ -42,9 +42,9 @@ import sys
 def solve(meal_cost, tip_percent, tax_percent):
     # Write your code here
     tip = meal_cost * tip_percent / 100
-    print("The tip is", tip)
+    # print("The tip is", tip)
     tax = meal_cost * tax_percent / 100
-    print( "The tax is", tax)
+    # print( "The tax is", tax)
     total = meal_cost + tip + tax
     
     print(round(total))
