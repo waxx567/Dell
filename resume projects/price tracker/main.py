fab468eb48fcf171b3768488ff772fe01ed1973b
Wayne McRae <waynem567@gmail.com>
2024-12-15 20:57:15 +0200

    Update main.py

--- resume projects/price tracker/main.py
+++ resume projects/price tracker/main.py
@@ -3,6 +3,7 @@ import requests
 import numpy as np
 import csv
 from datetime import datetime
+import re
 
 # link to scrape
 LINK = "https://www.bikekings.co.za/collections/fullface/products/shark-skwal-i3-hellcat-matte-kur?_pos=28&_fid=271f4b3e9&_ss=c"
@@ -14,7 +15,9 @@ def get_data(link):
     # parse data    
     soup = BeautifulSoup(response.text, "html.parser")
     # get price
-    price = soup.find("div",{"class":"product-page-info__price"}).find_all("span",{"class":"price"})
+    price_string = soup.find("div",{"class":"product-page-info__price"}).find_all("span",{"class":"price"})
+    price_search = re.compile("[^0-9]")
+    price = price_search.findall(price_string)
     
     print(price)
     return price
