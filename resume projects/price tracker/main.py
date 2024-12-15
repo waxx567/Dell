3f0a65a650ea304558e3155ecf3991e876c868df
Wayne McRae <waynem567@gmail.com>
2024-12-15 17:43:56 +0200

    Update main.py

--- resume projects/price tracker/main.py
+++ resume projects/price tracker/main.py
@@ -8,7 +8,9 @@ from datetime import datetime
 LINK = "https://www.bikekings.co.za/collections/fullface/products/shark-skwal-i3-hellcat-matte-kur?_pos=28&_fid=271f4b3e9&_ss=c"
 
 
-def get_data(link):
+if __name__ == "__main__":
+    # get_data(LINK)
+    link = LINK
     # get data
     response = requests.get(link)
     # parse data
@@ -20,3 +22,16 @@ def get_data(link):
     print("Scraping data from: " + link)
     print(price)
     print(title)
+
+# def get_data(link):
+    # get data
+    # response = requests.get(link)
+    # parse data
+    # soup = BeautifulSoup(response.content, "html.parser")
+    # get price
+    # price = soup.find("div", class_="product-page-info__price =").text.strip()
+    # get title
+    # title = soup.find("h1", class_="product__title").text.strip()
+    # print("Scraping data from: " + link)
+    # print(price)
+    # print(title)
