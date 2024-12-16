4086839bd505dc8f78125e657ad664f3b88242da
Wayne McRae <waynem567@gmail.com>
2024-12-16 11:16:09 +0200

    Update main.py

--- resume projects/voice recognition bot/main.py
+++ resume projects/voice recognition bot/main.py
@@ -3,3 +3,13 @@ import sounddevice as sd
 import numpy as np
 import scipy.io.wavfile as wav
 
+test_file = ''
+
+FILE_FROM_USER = 'RECORDING.wav'
+VOICE_FILE = 'VOICE.wav'
+
+r = sr.Recognizer()
+
+def recognize_speech_from_file(filename):
+    # open file
+    with
