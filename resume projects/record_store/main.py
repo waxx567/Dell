79fcdfda8f63b5d150524c5664f5939fc8cfe300
Wayne McRae <waynem567@gmail.com>
2024-12-16 15:57:25 +0200

    Update main.py

--- resume projects/record_store/main.py
+++ resume projects/record_store/main.py
@@ -1,7 +1,16 @@
 def main():
 
-    print("Hello, World!", end=" ")
+    # initialise record list
+    recordList = []
 
+    choice = 0
+    print("Welcome to the Record Store")
+    print("Please choose an option:")
+    print("1. Add a record")
+    print("2. Remove a record")
+    print("3. List all records")
+    print("4. Search for a record")
+    print("5. Quit")
 
 if __name__ == "__main__":
     main()
