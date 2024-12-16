13ea22a0988be957c30ec47ff816f516f177f3d4
Wayne McRae <waynem567@gmail.com>
2024-12-16 11:30:25 +0200

    Update conditional-intro.py

--- python/HackerRank/30 Days of Code/conditional-intro.py
+++ python/HackerRank/30 Days of Code/conditional-intro.py
@@ -12,43 +12,23 @@
 # If n is even and greater than 20, print Not Weird
 # Complete the stub code provided in your editor to print whether or not  is weird.
 
-#include <bits/stdc++.h>
+#!/bin/python3
 
-using namespace std;
+import math
+import os
+import random
+import re
+import sys
 
-string ltrim(const string &);
-string rtrim(const string &);
 
+if __name__ == '__main__':
+    N = int(input().strip())
 
-
-int main()
-{
-    string N_temp;
-    getline(cin, N_temp);
-
-    int N = stoi(ltrim(rtrim(N_temp)));
-
-    return 0;
-}
-
-string ltrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        s.begin(),
-        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
-    );
-
-    return s;
-}
-
-string rtrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-        s.end()
-    );
-
-    return s;
-}
+    if N % 2 != 0:
+        print("Weird")
+    elif N % 2 == 0 and 2 <= N <= 5:
+        print("Not Weird")
+    elif N % 2 == 0 and 6 <= N <= 20:
+        print("Weird")
+    elif N % 2 == 0 and N > 20:
+        print("Not Weird")
