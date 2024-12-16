d57bbb9dfe3ce59790b79acab03e1d8f5e930dd1
Wayne McRae <waynem567@gmail.com>
2024-12-16 14:15:57 +0200

    voice recognition bot

--- resume projects/voice recognition bot/main.py
+++ resume projects/voice recognition bot/main.py
@@ -5,37 +5,33 @@ import scipy.io.wavfile as wav
 
 test_file = 'over-radio-countdown_108bpm_A_minor.wav'
 
-FILE_FROM_USER = 'RECORDING.wav'
-VOICE_FILE = 'VOICE.wav'
+# FILE_FROM_USER = 'RECORDING.wav'
+# VOICE_FILE = 'VOICE.wav'
 
 r = sr.Recognizer()
 
 def recognize_speech_from_file(filename):
     # open file
     with sr.AudioFile(filename) as source:
-        # listen for audio
-        audio = r.listen(source)
+        # listen for audio and load to memory
+        audio = r.record(source)
         # recognize speech using Google Speech Recognition
-        try:
-            return r.recognize_google(audio)
-        except sr.UnknownValueError:
-            print("Google Speech Recognition could not understand audio")
-        except sr.RequestError as e:
-            print("Could not request results from Google Speech Recognition service; {0}".format(e))
+        text = r.recognize_google(audio)
+        return text
 
-def record_audio():
-    fs = 44100  # Sample rate
-    seconds = 5  # Duration of recording
-    myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
-    sd.wait()  # Wait until recording is finished
-    wav.write(VOICE_FILE, fs, myrecording)
-    return myrecording
+# def record_audio():
+#     fs = 44100  # Sample rate
+#     seconds = 5  # Duration of recording
+#     myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+#     sd.wait()  # Wait until recording is finished
+#     wav.write(VOICE_FILE, fs, myrecording)
+#     return myrecording
 
 def main():
-    record_audio()
+    # record_audio()
     result = recognize_speech_from_file(VOICE_FILE)
     print(result)
-    return result
+    # return result
 
 if __name__ == "__main__":
     main()
