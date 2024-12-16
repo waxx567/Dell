5e4bfe1725f53b17980fe7f1b80b4ade433312ec
Wayne McRae <waynem567@gmail.com>
2024-12-16 11:45:12 +0200

    Update main.py

--- resume projects/voice recognition bot/main.py
+++ resume projects/voice recognition bot/main.py
@@ -12,4 +12,14 @@ r = sr.Recognizer()
 
 def recognize_speech_from_file(filename):
     # open file
-    with
+    with sr.AudioFile(filename) as source:
+        # listen for audio
+        audio = r.listen(source)
+        # recognize speech using Google Speech Recognition
+        try:
+            return r.recognize_google(audio)
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
+            
