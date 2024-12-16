193c1fa2b761024d74ff92f53471a547abaff91a
Wayne McRae <waynem567@gmail.com>
2024-12-16 11:24:56 +0200

    Update conditional-intro.py

---
+++ python/HackerRank/30 Days of Code/conditional-intro.py
@@ -0,0 +1,54 @@
+# https://www.hackerrank.com/challenges/30-conditional-statements/problem?h_r=email&unlock_token=576dcb5f44804c35db83dd6fc207cf1b4a0917af
+
+# Objective
+# In this challenge, we learn about conditional statements. Check out the Tutorial tab for learning materials and an instructional video.
+
+# Task
+# Given an integer, , perform the following conditional actions:
+
+# If n is odd, print Weird
+# If n is even and in the inclusive range of 2 to 5, print Not Weird
+# If n is even and in the inclusive range of 6 to 20, print Weird
+# If n is even and greater than 20, print Not Weird
+# Complete the stub code provided in your editor to print whether or not  is weird.
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+
+
+int main()
+{
+    string N_temp;
+    getline(cin, N_temp);
+
+    int N = stoi(ltrim(rtrim(N_temp)));
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
