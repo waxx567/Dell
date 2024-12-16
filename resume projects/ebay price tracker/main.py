3540676dfcc557307d51c4a0ecf5fcf0912735e6
Wayne McRae <waynem567@gmail.com>
2024-12-16 10:02:45 +0200

    Update main.py

--- resume projects/ebay price tracker/main.py
+++ resume projects/ebay price tracker/main.py
@@ -11,14 +11,21 @@ LINK = "https://www.ebay.co.uk/sch/i.html?_from=R40&_trksid=p4432023.m570.l1313&
 
 
 def get_data(link):
+    """
+    Given a link to an ebay.co.uk search page, this function will scrape the prices of all
+        items on the page and return them as a list of floats.
+    """
     # get data
     response = requests.get(link)
     # parse data
     soup = BeautifulSoup(response.text, "html.parser")
+    # get list of items
     list_items = soup.find("ul", {"class": "srp-results"}).find_all("li", {"class": "s-item"})
 
+    # empty list to store prices
     prices = []
 
+    # get prices and add them to the list
     for item in list_items:
         text_price = item.find("span", {"class": "s-item__price"}).text
         if "to" in text_price:
