fa7fee87d32095afffd207ce9a2f2860d84a9a27
Wayne McRae <waynem567@gmail.com>
2024-12-19 20:15:27 +0200

    Update prompt.py

--- resume projects/voice_recognition_bot/prompt.py
+++ resume projects/voice_recognition_bot/prompt.py
@@ -1,27 +1,35 @@
 import speech_recognition as sr
+
 import sounddevice as sd
+
 import numpy as np
+
 import scipy.io.wavfile as wav
 
+
 test_file = 'over-radio-countdown_108bpm_A_minor.wav'
+
 FILE_FROM_USER = 'RECORDING.wav'
+
 VOICE_FILE = 'VOICE.txt'
+
 
 r = sr.Recognizer()
 
+
 def recognize_speech_from_file(filename):
-    # open file
+
     with sr.AudioFile(filename) as source:
-        # listen for audio and load to memory
         audio = r.record(source)
         text = r.recognize_google(audio)
         return text
 
 def record_audio(filename):
-    fs = 44100  # Sample rate
-    seconds = 5  # Duration of recording
+
+    fs = 44100  
+    seconds = 5
     myrecording = sd.rec(seconds * fs, samplerate=fs, channels=1, dtype='int32')
-    sd.wait()  # Wait until recording is finished
+    sd.wait()
     print("Recording saved")
     sd.play(myrecording, fs)
     sd.wait()
@@ -29,12 +37,13 @@ def record_audio(filename):
     wav.write(filename, fs, myrecording)
 
 def save_to_file(text, filename):
+
     with open(filename, 'w') as f:
         f.write(text)
 
 
 def main():
-    # print(recognize_speech_from_file(test_file))
+
     print("Please record your voice for 5 seconds")
     record_audio(FILE_FROM_USER)
     voice_to_text = recognize_speech_from_file(FILE_FROM_USER)
