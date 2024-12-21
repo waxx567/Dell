16774ac1366c35af3a8fdbb7b93964662d4dca36
Wayne McRae <waynem567@gmail.com>
2024-12-21 18:38:45 +0200

    Update main.py

--- resume projects/games/space_dodge/main.py
+++ resume projects/games/space_dodge/main.py
@@ -5,3 +5,14 @@ import random
 WIDTH, HEIGHT = 800, 600
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Dodge")
+
+def main():
+    run = True
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+    pygame.quit()
