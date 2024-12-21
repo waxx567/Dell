5b2c67fc552b55cdeb747cad4d80124ca00ce142
Wayne McRae <waynem567@gmail.com>
2024-12-21 10:12:31 +0200

    Update app.py

--- resume projects/flask_website/app.py
+++ resume projects/flask_website/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -9,6 +10,10 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route("/services")
+def services():
+    return render_template("services.html")
 
 
 if __name__ == "__main__":
