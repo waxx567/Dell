18421357c96469b874b5d1d116280a6300fd72b7
Wayne McRae <waynem567@gmail.com>
2024-12-21 18:45:48 +0200

    Update main.py

--- resume projects/games/space_dodge/main.py
+++ resume projects/games/space_dodge/main.py
@@ -6,6 +6,8 @@ WIDTH, HEIGHT = 800, 600
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Dodge")
 
+BG = pygame.transform.scale(pygame.image.load("bg.jpeg"), (WIDTH, HEIGHT))
+
 def main():
     run = True
 
