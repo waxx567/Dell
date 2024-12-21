9374bc1dbc4483bb1ce32f397a4b9ca12648cd3f
Wayne McRae <waynem567@gmail.com>
2024-12-21 18:57:42 +0200

    Update main.py

--- resume projects/games/space_dodge/main.py
+++ resume projects/games/space_dodge/main.py
@@ -6,7 +6,7 @@ WIDTH, HEIGHT = 800, 600
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Dodge")
 
-BG = pygame.image.load("bg.jpeg")
+BG = pygame.transform.scale(pygame.image.load("bg.jpeg"), (WIDTH, HEIGHT))
 
 def draw():
     WIN.blit(BG, (0, 0))
