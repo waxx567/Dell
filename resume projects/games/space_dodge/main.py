849e66538de6d7526d19b53d15a8b681a426d1d2
Wayne McRae <waynem567@gmail.com>
2024-12-21 18:55:00 +0200

    Update main.py

--- resume projects/games/space_dodge/main.py
+++ resume projects/games/space_dodge/main.py
@@ -2,7 +2,7 @@ import pygame
 import time
 import random
 
-WIDTH, HEIGHT = 800, 600
+WIDTH, HEIGHT = 1000, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Dodge")
 
