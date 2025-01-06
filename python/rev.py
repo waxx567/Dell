c91142aeca56ebff5586e3644f2fe0d2206451b6
Wayne McRae <waynem567@gmail.com>
2025-01-06 10:43:51 +0200

    Create rev.py

---
+++ python/rev.py
@@ -0,0 +1,21 @@
+# def reverse_number(num):
+  # Reverse the number
+#   num = str(num)
+#   reverse = num[::-1]
+#   print(reverse)
+#   reverse = ''.join(reversed(num))
+  # Return the number
+#   return reverse
+
+## Example usage:
+# print(int(reverse_number(1223))) # Output: 3221
+# print(int(reverse_number(987654321))) # Output: 123456789
+
+
+def oppositeNumber(num):
+    return -num
+
+# Example usage:
+print(oppositeNumber(3221))
+print(oppositeNumber(5)) # Output: -5
+print(oppositeNumber(-7)) # Output: 7
